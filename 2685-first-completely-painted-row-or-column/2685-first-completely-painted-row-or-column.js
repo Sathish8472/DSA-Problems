120c1b6005792034d17617f73a997e9fe2a65238
Sathish <sathish8472@gmail.com>
2025-01-20 08:54:20 +0000

    Time: 160 ms (25%), Space: 89.1 MB (16.67%) - LeetHub

---
+++ 2685-first-completely-painted-row-or-column/2685-first-completely-painted-row-or-column.js
@@ -0,0 +1,102 @@
+/**
+ * @param {number[]} arr
+ * @param {number[][]} mat
+ * @return {number}
+ */
+
+
+// Approach 2: Hash table
+
+var firstCompleteIndex = function (arr, mat) {
+
+    const m = mat.length;
+    const n = mat[0].length;
+
+    const positionMap = new Map();
+    for(let i = 0; i < m; i++){
+        for(let j = 0; j < n; j++){
+            positionMap.set(mat[i][j], [i, j]);
+        }
+    }
+
+    const rowPaintCount = new Array(m).fill(0);
+    const colPaintCount = new Array(n).fill(0);
+    
+    for(let i = 0 ; i < m * n; i++){
+        //console.log(positionMap);
+        const [row, col] = positionMap.get(arr[i]);
+
+        rowPaintCount[row]++;
+        colPaintCount[col]++;
+
+        if(rowPaintCount[row] === n || colPaintCount[col] === m){
+            return i;
+        }
+    }
+
+    return -1;
+}
+
+
+ // Approach 1: Brute force
+var firstCompleteIndex1 = function (arr, mat) {
+    const len = arr.length;
+    const m = mat.length;
+    const n = mat[0].length;
+    let index = 0;
+
+    const isAnyRowPainted = (mat) => {
+        
+        for (let x = 0; x < m; x++) {
+            let count = 0;
+            for (let y = 0; y < n; y++) {
+                if (mat[x][y] === 0) {
+                    count++;
+                }
+            }
+
+            if (count === n) {
+                return true;
+            }
+        }
+        
+        console.log(`Mat in row: ${mat} `);
+        return false;
+    }
+
+    function isAnyColPainted(mat) {
+        for (let x = 0; x < n; x++) {
+            let count = 0;
+            for (let y = 0; y < m; y++) {
+                if (mat[y][x] === 0) {
+                    count++;
+                }
+            }
+
+            if (count === m) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    for (let i = 0; i < len; i++) {
+        let paintValue = arr[i];
+
+        for (let j = 0; j < m; j++) {
+            for (let k = 0; k < n; k++) {
+                if (mat[j][k] == paintValue) {
+                    mat[j][k] = 0;
+                }
+            }
+        }
+
+        if (isAnyRowPainted(mat) || isAnyColPainted(mat)) {
+            index = i;
+            break;
+        }
+    }
+
+    return index;
+};
