db573a3e10d973adb39618aae78bf21f7460e4ec
Sathish <sathish8472@gmail.com>
2025-01-21 16:50:09 +0000

    Time: 0 ms (100%), Space: 48.8 MB (61.81%) - LeetHub

--- 0198-house-robber/0198-house-robber.js
+++ 0198-house-robber/0198-house-robber.js
@@ -2,15 +2,15 @@
  * @param {number[]} nums
  * @return {number}
  */
-var rob = function(nums) {
+var rob1 = function (nums) {
     const n = nums.length;
     let prev = nums[0];
     let prev2 = 0;
 
-    for(let i = 1; i < n; i++){
+    for (let i = 1; i < n; i++) {
         let pick = nums[i];
 
-        if(i > 1)
+        if (i > 1)
             pick += prev2;
 
         let notPick = 0 + prev;
@@ -21,4 +21,27 @@ var rob = function(nums) {
     }
 
     return prev;
+};
+
+var rob = function (nums) {
+
+    const solve = (ind, nums, dp) => {
+
+        if (ind === 0) return nums[ind];
+        if (ind < 0) return 0;
+
+        if (dp[ind] !== -1)
+            return dp[ind];
+
+        let pick = nums[ind] + solve(ind - 2, nums, dp);
+        let notPick = 0 + solve(ind - 1, nums, dp);
+
+        dp[ind] = Math.max(pick, notPick);
+        return dp[ind];
+    };
+
+    const n = nums.length;
+    let dp = new Array(n).fill(-1);
+    return solve(n - 1, nums, dp);
+
 };
