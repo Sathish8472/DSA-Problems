f4c9f3d5f5e251118516a5ee339e0291cd4cb205
Sathish <sathish8472@gmail.com>
2025-01-22 17:10:02 +0000

    Time: 404 ms (47.37%), Space: 160.5 MB (18.42%) - LeetHub

--- 1876-map-of-highest-peak/1876-map-of-highest-peak.js
+++ 1876-map-of-highest-peak/1876-map-of-highest-peak.js
@@ -46,3 +46,46 @@ var highestPeak = function (isWater) {
 
     return height;
 };
+
+// DP
+var highestPeak1 = function (isWater) {
+    const m = isWater.length;
+    const n = isWater[0].length;
+    const INF = Infinity;
+
+    // Initialize height matrix
+    const height = Array.from({ length: m }, () => Array(n).fill(INF));
+    
+    // Set water cells to height 0
+    for (let i = 0; i < m; i++) {
+        for (let j = 0; j < n; j++) {
+            if (isWater[i][j] === 1) {
+                height[i][j] = 0;
+            }
+        }
+    }
+
+    // Forward pass: top-left to bottom-right
+    for (let i = 0; i < m; i++) {
+        for (let j = 0; j < n; j++) {
+            if (height[i][j] !== 0) { // Skip water cells
+                const top = i > 0 ? height[i - 1][j] : INF;
+                const left = j > 0 ? height[i][j - 1] : INF;
+                height[i][j] = Math.min(height[i][j], Math.min(top, left) + 1);
+            }
+        }
+    }
+
+    // Backward pass: bottom-right to top-left
+    for (let i = m - 1; i >= 0; i--) {
+        for (let j = n - 1; j >= 0; j--) {
+            if (height[i][j] !== 0) { // Skip water cells
+                const bottom = i < m - 1 ? height[i + 1][j] : INF;
+                const right = j < n - 1 ? height[i][j + 1] : INF;
+                height[i][j] = Math.min(height[i][j], Math.min(bottom, right) + 1);
+            }
+        }
+    }
+
+    return height;
+};
