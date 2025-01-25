4f96635453d5463caec99d89f69b28d731b05705
Sathish <sathish8472@gmail.com>
2025-01-25 05:53:26 +0000

    Time: 11 ms (36.86%), Space: 23.2 MB (12.49%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -1,8 +1,21 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        filtered_chars = ''.join(c.lower() for c in s if c.isalnum())
-        left, right = 0, len(filtered_chars) - 1
+        filtered_chars = ''.join(c.lower() for c in s if c.isalnum())  # this is filtering the special characters
+        n = len(filtered_chars)
         
+        for i in range(n // 2):
+            if(filtered_chars[i] != filtered_chars[n - 1 - i]):
+                return False;
+
+        return True;
+        
+
+class Solution1:
+    def isPalindrome(self, s: str) -> bool:
+        filtered_chars = ''.join(c.lower() for c in s if c.isalnum())  # this is filtering the special characters
+
+        '''        
+        left, right = 0, len(filtered_chars) - 1
         while(left < right):
             if(filtered_chars[left] != filtered_chars[right]):
                 return False
@@ -10,4 +23,4 @@ class Solution:
             right -= 1
 
         return True
-        
+        '''
