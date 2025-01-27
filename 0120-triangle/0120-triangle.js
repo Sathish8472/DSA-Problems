49ffe283dd7ab874efcb50f247991d26587a59f4
Sathish <sathish8472@gmail.com>
2025-01-27 08:47:33 +0000

    Time: 3 ms (55.67%), Space: 51.1 MB (37.45%) - LeetHub

--- 0120-triangle/0120-triangle.js
+++ 0120-triangle/0120-triangle.js
@@ -3,8 +3,34 @@
  * @return {number}
  */
 
-// Memoization
+// Tabulation, TC: O(N * N), SP: O(N * N)
 var minimumTotal = function (triangle) {
+    const n = triangle.length;
+
+    let front = new Array(n).fill(0);
+    let cur = new Array(n).fill(0);
+
+    for (let j = 0; j < n; j++) {
+        front[j] = triangle[n - 1][j];
+    }
+
+    for (let i = n - 2; i >= 0; i--) {
+        for (let j = 0; j <= i; j++) {
+            const down = triangle[i][j] + front[j];
+            const diagonal = triangle[i][j] + front[j + 1];
+
+            cur[j] = Math.min(down, diagonal);
+        }
+
+        front = [...cur];
+    }
+
+    return front[0];
+};
+
+
+// Memoization, TC: O(N * N), SP: O(N * N)
+var minimumTotal1 = function (triangle) {
     const n = triangle.length;
 
     const dp = new Array(n).fill().map(() => new Array(n).fill(-1));
