128d04f294e1ac7351ca27e99d82fb8239fa5ce1
Sathish <sathish8472@gmail.com>
2025-02-02 06:51:19 +0000

    Time: 462 ms (58.23%), Space: 107 MB (48.82%) - LeetHub

--- 2121-find-if-path-exists-in-graph/2121-find-if-path-exists-in-graph.py
+++ 2121-find-if-path-exists-in-graph/2121-find-if-path-exists-in-graph.py
@@ -4,7 +4,6 @@ class DisjointSet:
         self.rank = [0] * n
 
     def find(self, u):
-
         if self.parent[u] != u:
             self.parent[u] = self.find(self.parent[u])
 
@@ -24,11 +23,61 @@ class DisjointSet:
                 self.rank[rootU] += 1   
 
 class Solution:
-    def validPath(self, n: int, edges: List[List[int]], source: int, destination: int) -> bool:
+    # Disjoint solution: Optimized solution
+    def validPath_1(self, n: int, edges: List[List[int]], source: int, destination: int) -> bool:
         ds = DisjointSet(n)
 
         for u, v in edges:
             ds.union(u, v)
 
         return ds.find(source) == ds.find(destination)
+
+
+
+    # DFS & BFS approach
+    def validPath(self, n: int, edges: List[List[int]], source: int, destination: int) -> bool:
+        if source == destination:
+            return True
+
+        """Build adjacency list"""
+        graph= {i: [] for i in range(n)}
+        for u, v in edges:
+            graph[u].append(v)
+            graph[v].append(u)
+
+        visited = set()
+       
+        # BFS
+        def bfs(source):
+            queue = deque([source])
+            visited = set([source])
+
+            while queue:
+                node = queue.popleft()
+                if node == destination:
+                    return True
+                
+                for neighbor in graph[node]:
+                    if neighbor not in visited:
+                        visited.add(neighbor)
+                        queue.append(neighbor)
+
+            return False
+
+        # DFS approach
+        def dfs(node):
+            
+            if node == destination:
+                return True
+            visited.add(node)
+
+            for neighbor in graph[node]:
+                if neighbor not in visited:
+                    if dfs(neighbor):
+                        return True
+
+            return False
+        
+        return bfs(source)
+
         
