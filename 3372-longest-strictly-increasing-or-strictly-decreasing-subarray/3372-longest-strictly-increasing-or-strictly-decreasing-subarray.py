3a5512260916f2bc7c10ad039f8d496bfeb0ff07
Sathish <sathish8472@gmail.com>
2025-02-03 09:30:00 +0000

    Time: 3 ms (51.94%), Space: 17.7 MB (47.33%) - LeetHub

--- 3372-longest-strictly-increasing-or-strictly-decreasing-subarray/3372-longest-strictly-increasing-or-strictly-decreasing-subarray.py
+++ 3372-longest-strictly-increasing-or-strictly-decreasing-subarray/3372-longest-strictly-increasing-or-strictly-decreasing-subarray.py
@@ -1,8 +1,34 @@
+# Optimized solution: Single iteration
+
 class Solution:
     def longestMonotonicSubarray(self, nums: List[int]) -> int:
+        max_length = inc_length = dec_length = 1
+        n = len(nums)
+
+        for i in range(n - 1):
+            if nums[i + 1] > nums[i]:  # Increasing
+                inc_length += 1
+                dec_length = 1
+            elif nums[i + 1] < nums[i]:
+                dec_length += 1
+                inc_length = 1
+            else:
+                inc_length = dec_length = 1
+
+            max_length = max(max_length, inc_length, dec_length)
+                
+        return max_length
+
+
+# Brute force approach
+# Time: O(n ^ 2)
+
+class Solution_1:
+    def longestMonotonicSubarray_1(self, nums: List[int]) -> int:
         max_length = 1
         n = len(nums)
         
+        # Increasing sub array
         for start in range(n):
             current_length = 1
             for pos in range(start + 1, n):
@@ -13,7 +39,7 @@ class Solution:
 
             max_length = max(max_length, current_length)
 
-
+        # Decreasing sub array
         for start in range(n):
             current_length = 1
             for pos in range(start + 1, n):
