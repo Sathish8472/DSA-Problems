aa2305b98bf8b4f57bc2cc7d9e72808cf81a3e2b
Sathish <sathish8472@gmail.com>
2025-02-08 05:59:06 +0000

    Time: 27 ms (88.56%), Space: 28.7 MB (5.32%) - LeetHub

--- 2432-number-of-zero-filled-subarrays/2432-number-of-zero-filled-subarrays.py
+++ 2432-number-of-zero-filled-subarrays/2432-number-of-zero-filled-subarrays.py
@@ -1,10 +1,26 @@
 class Solution:
-    def zeroFilledSubarray(self, nums: List[int]) -> int:
 
+    # Single pass count
+    def zeroFilledSubarray2(self, nums: List[int]) -> int:
+        count = total = 0
+
+        for num in nums:
+            if num == 0:
+                count += 1
+                total += count
+            else:
+                count = 0
+
+        return total
+
+
+
+    # List to store zero counts
+    def zeroFilledSubarray(self, nums: List[int]) -> int:
         zeros = []
         count = 0
-        for i in range(len(nums)):
-            if nums[i] == 0:
+        for num in nums:
+            if num == 0:
                 count += 1
             else:
                 if count > 0:
@@ -14,13 +30,4 @@ class Solution:
         if count > 0:
             zeros.append(count)
 
-
-        result = 0
-        for i in zeros:
-            result += i * (i + 1) // 2
-
-        return result
-
-                
-
-        
+        return sum(i * (i + 1) // 2 for i in zeros)
