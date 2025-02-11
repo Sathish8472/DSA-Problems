a7b91404d0f7293644cf78abd9a742723c13259d
Sathish <sathish8472@gmail.com>
2025-02-11 05:47:36 +0000

    Time: 0 ms (100%), Space: 17.9 MB (22.99%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.py
+++ 0392-is-subsequence/0392-is-subsequence.py
@@ -1,5 +1,7 @@
 class Solution:
-    def isSubsequence(self, s: str, t: str) -> bool:
+
+    # Two-Pointer Approach (Iterative)
+    def isSubsequence_1(self, s: str, t: str) -> bool:
         index = 0
 
         for ch in t:
@@ -7,3 +9,11 @@ class Solution:
                 index += 1
 
         return index == len(s)
+
+
+    def isSubsequence(self, s: str, t: str) -> bool:
+        t_iter = iter(t)
+        return all(char in t_iter for char in s)
+
+
+    # Can be solved using DP, Recursive, Binary Search and others
