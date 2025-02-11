6d285764ab00a70c5739a51c9656f8c5e101c2e5
Sathish <sathish8472@gmail.com>
2025-02-11 05:17:22 +0000

    Time: 0 ms (100%), Space: 17.9 MB (51.53%) - LeetHub

--- 2021-remove-all-occurrences-of-a-substring/2021-remove-all-occurrences-of-a-substring.py
+++ 2021-remove-all-occurrences-of-a-substring/2021-remove-all-occurrences-of-a-substring.py
@@ -1,21 +1,28 @@
 class Solution:
+
+    # Efficient Stack Approach
+    # Time: O(N), Space: O(N)
     def removeOccurrences_1(self, s: str, part: str) -> str:
         stack = []
 
         for ch in s:
             stack.append(ch)
 
+            # Check if the top len(part) characters form part
             if len(stack) >= len(part) and "".join(stack[-len(part) :]) == part:
                 del stack[-len(part) :]
 
         return "".join(stack)
-    
+
+
 
 
     # Brute Force
+    # Time: O(N * M)
+    # Space: O(N)
     def removeOccurrences(self, s: str, part: str) -> str:
         while part in s:
-            index = s.find(part)
-            s = s[:index] + s[index + len(part):]
+            index = s.find(part)  # O(M) time taken
+            s = s[:index] + s[index + len(part) :]     # Extra Space for slicing
 
         return s
