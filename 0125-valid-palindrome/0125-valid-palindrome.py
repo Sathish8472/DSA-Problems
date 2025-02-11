88375d07a7896fc783f69dd828548f643363f64c
Sathish <sathish8472@gmail.com>
2025-02-11 06:14:52 +0000

    Time: 12 ms (19.79%), Space: 23.2 MB (9.81%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -1,15 +1,24 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
+        filtered_chars = ''.join(c.lower() for c in s if c.isalnum())  # this is filtering the special characters
+        n = len(filtered_chars)
+        
+        for i in range(n // 2):
+            if(filtered_chars[i] != filtered_chars[n - 1 - i]):
+                return False;
 
-        filtered_chars = "".join(c.lower() for c in s if c.isalnum())
-        print(filtered_chars)
+        return True;
+        
 
+# Two pointer solution
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        filtered_chars = ''.join(c.lower() for c in s if c.isalnum())  # this is filtering the special characters
+        print("dsfsd")
         left, right = 0, len(filtered_chars) - 1
-
-        while left < right:
-            if filtered_chars[left] != filtered_chars[right]:
+        while(left < right):
+            if(filtered_chars[left] != filtered_chars[right]):
                 return False
-
             left += 1
             right -= 1
 
