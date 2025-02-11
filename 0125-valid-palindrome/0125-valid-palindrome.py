4e3e3583b382e080f3dbba8f345869c271b4105a
Sathish <sathish8472@gmail.com>
2025-02-11 06:16:10 +0000

    Time: 7 ms (81.2%), Space: 23.4 MB (5.81%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -1,24 +1,23 @@
 class Solution:
-    def isPalindrome(self, s: str) -> bool:
-        filtered_chars = ''.join(c.lower() for c in s if c.isalnum())  # this is filtering the special characters
+    def isPalindrome_1(self, s: str) -> bool:
+        filtered_chars = "".join(c.lower() for c in s if c.isalnum())
         n = len(filtered_chars)
-        
+
         for i in range(n // 2):
-            if(filtered_chars[i] != filtered_chars[n - 1 - i]):
-                return False;
-
-        return True;
-        
-
-# Two pointer solution
-class Solution:
-    def isPalindrome(self, s: str) -> bool:
-        filtered_chars = ''.join(c.lower() for c in s if c.isalnum())  # this is filtering the special characters
-        print("dsfsd")
-        left, right = 0, len(filtered_chars) - 1
-        while(left < right):
-            if(filtered_chars[left] != filtered_chars[right]):
+            if filtered_chars[i] != filtered_chars[n - 1 - i]:
                 return False
+
+        return True
+
+    # Two pointer solution
+    def isPalindrome(self, s: str) -> bool:
+        filtered_chars = "".join(c.lower() for c in s if c.isalnum())
+        left, right = 0, len(filtered_chars) - 1
+
+        while left < right:
+            if filtered_chars[left] != filtered_chars[right]:
+                return False
+
             left += 1
             right -= 1
 
