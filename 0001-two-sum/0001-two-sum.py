8e0ac23589916597a648aba7745594134810a18c
Sathish <sathish8472@gmail.com>
2025-02-12 17:36:42 +0000

    Time: 0 ms (100%), Space: 19.1 MB (10.92%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -12,8 +12,10 @@ class Solution:
 
     # Brute force, Time: O(N ^ 2)
     def twoSum_1(self, nums: List[int], target: int) -> List[int]:
-        for i in range(len(nums)):
-            for j in range(i + 1, len(nums)):
+        n = len(nums)
+
+        for i in range(n):
+            for j in range(i + 1, n):
                 if nums[i] + nums[j] == target:
                     return [i, j]
                 
