cabd1ed94af47375dcd5209dbd4d3d5a584253cf
Sathish <sathish8472@gmail.com>
2025-02-12 08:02:40 +0000

    Time: 10 ms (9.04%), Space: 17.9 MB (66.1%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.py
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.py
@@ -1,17 +1,55 @@
 class Solution:
-
-    # In-place
-    def reverseWords_2(self, s: str) -> str:
-        s = s.strip()
+    # In-place Efficient Approach
+    def reverseWords(self, s: str) -> str:
         s = list(s)
         self.reverse(s, 0, len(s) - 1)
-        print(s)
+        self.reverse_each_word(s)
+
+        return ''.join(self.clean_spaces(s))
+
+    def reverse(self, s, left, right):
+        while left < right:
+            s[left], s[right] = s[right], s[left]
+            left += 1
+            right -= 1
+
+    def reverse_each_word(self, s):
+        n = len(s)
+        start = 0
+        while start < n:
+            while start < n and s[start] == ' ':
+                start += 1
+            
+            end = start
+            while end < n and s[end] != ' ':
+                end += 1
+            
+            self.reverse(s, start, end - 1)
+
+            start = end
+
+    def clean_spaces(self, s):
+        n = len(s)
+        result = []
+        in_word = False
+        for i in range(n):
+            if s[i] != ' ':
+                if not in_word:
+                    result.append(' ') if result else None  # Add a single space before a new word
+                    in_word = True
+                result.append(s[i])
+            else:
+                in_word = False
+        return result
+
+
 
     # Brute
-    def reverseWords_1(self, s: str) -> str:
+    # Time: O(N), Space: O(N)
+    def reverseWords_2(self, s: str) -> str:
         words = [word for word in s.split(" ") if word]
         return " ".join(reversed(words))
 
 
-    def reverseWords(self, s: str) -> str:
+    def reverseWords_1(self, s: str) -> str:
         return " ".join(reversed(s.split()))
