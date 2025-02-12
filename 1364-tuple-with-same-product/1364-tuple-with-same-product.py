9fec3c097a7e124a66d77aa9a18ee60168c75248
Sathish <sathish8472@gmail.com>
2025-02-12 17:57:54 +0000

    Time: 492 ms (28.18%), Space: 46.5 MB (24.76%) - LeetHub

--- 1364-tuple-with-same-product/1364-tuple-with-same-product.py
+++ 1364-tuple-with-same-product/1364-tuple-with-same-product.py
@@ -2,7 +2,6 @@ class Solution:
     # Optimized Brute Force with Hash Map
     def tupleSameProduct(self, nums: List[int]) -> int: 
         product_map = defaultdict(int)
-
         n = len(nums)
         
         for i in range(n):
@@ -10,6 +9,7 @@ class Solution:
                 product = nums[i] * nums[j]
                 product_map[product] += 1
 
+        print(product_map)
         count = 0
 
         for pair_count in product_map.values():
@@ -18,6 +18,8 @@ class Solution:
 
         return count
                 
+
+
     # Brute Force Approach
     # Time: O(n ^ 4)
     def tupleSameProduct_1(self, nums: List[int]) -> int:
@@ -31,6 +33,5 @@ class Solution:
                         if i != j and i != k and i != l and j != k and j != l and k != l:
                             if nums[i] * nums[j] == nums[k] * nums[l]:
                                 count += 1
-
         
         return count
