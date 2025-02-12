a3e9ddc4c46df1b0947c1074dc81c90ba36f01ad
Sathish <sathish8472@gmail.com>
2025-02-12 18:21:43 +0000

    Time: 172 ms (5.09%), Space: 27 MB (31.91%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
@@ -1,15 +1,67 @@
 class Solution:
-    def maxProfit(self, prices: List[int]) -> int:
 
+    # One pass Efficient Approach
+    def maxProfit_22(self, prices: List[int]) -> int:
         max_profit = 0
-        min_price = prices[0]
+        min_price = float('inf')
 
         for price in prices:
             if price < min_price:
                 min_price = price
+            else:
+                profit = price - min_price
+                max_profit = max(max_profit, profit)
+                
+        return max_profit
 
-            profit = price - min_price
-            max_profit = max(max_profit, profit)
+    
+
+    def maxProfit(self, prices: List[int]) -> int:
+        n = len(prices)
+        dp = [0] * n
+        min_price = prices[0]
+
+        for i in range(1, n):
+            min_price = min(min_price, prices[i])
+            dp[i] =  max(dp[i - 1], prices[i] - min_price)
+
+        return dp[-1]
+
+
+
+    # DP, Not Efficient
+    # Time : O(N)
+    def maxProfit33(self, prices: List[int]) -> int:
+        n = len(prices)
+        if n == 0:
+            return 0
+
+        dp = [0] * n
+        min_price = prices[0]
+
+        for i in range(1, n):
+            min_price = min(min_price, prices[i])
+            dp[i] = max(dp[i - 1], prices[i] - min_price)
+
+        return dp[-1]
+
+
+
+
+
+
+
+    # Brute force
+    # Time: O(n ^ 2)
+    def maxProfit_1(self, prices: List[int]) -> int:
+        max_profit = 0
+        n = len(prices)
+
+        for i in range(n):
+            for j in range(i + 1, n):
+                if prices[j] > prices[i]:
+                    profit = prices[j] - prices[i]
+                    max_profit = max(max_profit, profit)
 
         return max_profit
         
