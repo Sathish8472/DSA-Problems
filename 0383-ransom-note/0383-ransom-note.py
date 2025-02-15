342c21d5e4799af8ac23bbde15e4528a85ae3d40
Sathish <sathish8472@gmail.com>
2025-02-15 07:16:13 +0000

    Time: 13 ms (81.26%), Space: 18.3 MB (13.21%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.py
+++ 0383-ransom-note/0383-ransom-note.py
@@ -1,6 +1,22 @@
 class Solution:
+
     def canConstruct(self, ransomNote: str, magazine: str) -> bool:
         
+        ransomNote_count = Counter(ransomNote)
+        magazine_count = Counter(magazine)
+
+        for char, count in ransomNote_count.items():
+            if magazine_count[char] < count:
+                return False
+            print(char)
+            print(count)
+
+        print("---")
+        print(magazine_count)
+        return True
+
+    # Brute Force 
+    def canConstruct_1(self, ransomNote: str, magazine: str) -> bool:
         mFreq_map = {}
         for ch in magazine:
             mFreq_map[ch] = mFreq_map.get(ch, 0) + 1
