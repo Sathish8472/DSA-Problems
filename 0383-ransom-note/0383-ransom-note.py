ba74d498665b6c06aefeed786e16879d0e67d534
Sathish <sathish8472@gmail.com>
2025-02-15 07:12:22 +0000

    Time: 22 ms (51.95%), Space: 17.9 MB (53.4%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.py
+++ 0383-ransom-note/0383-ransom-note.py
@@ -1,26 +1,13 @@
 class Solution:
     def canConstruct(self, ransomNote: str, magazine: str) -> bool:
+        
         mFreq_map = {}
-
         for ch in magazine:
-            if ch in mFreq_map:
-                mFreq_map[ch] += 1
-            else:
-                mFreq_map[ch] = 1
-        
-        print(mFreq_map)
+            mFreq_map[ch] = mFreq_map.get(ch, 0) + 1
+
         for ch in ransomNote:
-            print(ch)
-            if ch in mFreq_map:
-                if mFreq_map[ch] > 0:
-                    mFreq_map[ch] -= 1
-                else:
-                    return False
-            else:
+            if mFreq_map.get(ch, 0) == 0:
                 return False
-        
-        print(mFreq_map)
+            mFreq_map[ch] -= 1
+
         return True
-
-
-        
