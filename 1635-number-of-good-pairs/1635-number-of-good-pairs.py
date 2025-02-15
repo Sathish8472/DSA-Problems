d7a10be25d82dc9825616f0cca43af6ee816f685
Sathish <sathish8472@gmail.com>
2025-02-15 06:17:19 +0000

    Time: 0 ms (100%), Space: 17.8 MB (30.57%) - LeetHub

--- 1635-number-of-good-pairs/1635-number-of-good-pairs.py
+++ 1635-number-of-good-pairs/1635-number-of-good-pairs.py
@@ -2,27 +2,25 @@ class Solution:
 
     def numIdenticalPairs(self, nums: List[int]) -> int:
         count = 0
-        hashmap = {}
+        freq_map = defaultdict(int)
 
         for num in nums:
-            print(hashmap)
-            if num in hashmap:
-                count += hashmap[num]
-                hashmap[num] += 1
-            else:
-                hashmap[num] = 1
-
+            count += freq_map[num]
+            freq_map[num] += 1
 
         return count
 
+
+
+
+    # Brute-force
     def numIdenticalPairs_1(self, nums: List[int]) -> int:
         count = 0
         n = len(nums)
-        
+
         for i in range(n):
             for j in range(i + 1, n):
                 if nums[i] == nums[j]:
                     count += 1
-            
+
         return count
-        
