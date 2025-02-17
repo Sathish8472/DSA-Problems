07f667285346ec0118a009c936fef07904c727fd
Sathish <sathish8472@gmail.com>
2025-02-17 08:30:14 +0000

    Time: 4 ms (12.18%), Space: 18.2 MB (14.61%) - LeetHub

---
+++ 0046-permutations/0046-permutations.py
@@ -0,0 +1,46 @@
+class Solution:
+
+    # Optmized code
+    # Time: O(N!), Space: O(N)
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        result = []
+
+        def backtrack(cur_perm):
+            if len(cur_perm) == len(nums):
+                result.append(cur_perm[:])
+                return
+
+            for num in nums:
+                if num in cur_perm:
+                    continue
+                cur_perm.append(num)
+                backtrack(cur_perm)
+                cur_perm.pop()
+
+        backtrack([])
+        return result
+
+
+
+    def permute1(self, nums: List[int]) -> List[List[int]]:
+        result = []
+        ds = []
+        freq = [False] * len(nums)
+
+        def generatePermute():
+            if len(ds) == len(nums):
+                result.append(
+                    ds[:]
+                )  # Adds a copy of the current permutation to result (to avoid reference issues).
+                return
+
+            for i, num in enumerate(nums):
+                if not freq[i]:
+                    freq[i] = True
+                    ds.append(num)
+                    generatePermute()
+                    freq[i] = False
+                    ds.pop()
+
+        generatePermute()
+        return result
