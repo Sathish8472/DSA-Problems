4e5d9a9a613945ff9b1f1b7c641ccac58aaaa1f9
Sathish <sathish8472@gmail.com>
2025-02-17 06:32:05 +0000

    Time: 38 ms (58.27%), Space: 17.8 MB (83.06%) - LeetHub

---
+++ 1160-letter-tile-possibilities/1160-letter-tile-possibilities.py
@@ -0,0 +1,18 @@
+class Solution:
+    def numTilePossibilities(self, tiles: str) -> int:
+        count = Counter(tiles)
+        print(count)
+
+        def backtrack():
+            total = 0
+            for ch in count:
+                if count[ch] > 0:
+                    count[ch] -= 1
+                    total += 1 + backtrack()
+                    count[ch] += 1
+            
+            return total
+
+        return backtrack()
+
+        
