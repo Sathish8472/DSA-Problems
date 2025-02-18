c9548b9ba73f2b15e5da9047bc62f06d6ab105d8
Sathish <sathish8472@gmail.com>
2025-02-18 08:00:37 +0000

    Time: 1 ms (35.67%), Space: 17.8 MB (38.79%) - LeetHub

---
+++ 0216-combination-sum-iii/0216-combination-sum-iii.py
@@ -0,0 +1,19 @@
+class Solution:
+    def combinationSum3(self, k: int, n: int) -> List[List[int]]:
+        result = []
+
+        def backtrack(start, current, target):
+            if len(current) == k and target == 0:
+                result.append(current[:])
+                return
+
+            if len(current) > k or target < 0:
+                return
+
+            for i in range(start, 10):
+                current.append(i)
+                backtrack(i + 1, current, target - i)
+                current.pop()
+
+        backtrack(1, [], n)
+        return result
