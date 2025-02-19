2ec7428819a589e5ae7e811b309a09a451abf341
Sathish <sathish8472@gmail.com>
2025-02-19 09:17:24 +0000

    Time: 0 ms (100%), Space: 17.7 MB (91.8%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
@@ -2,10 +2,10 @@ class Solution:
     def letterCombinations(self, digits: str) -> List[str]:
         result = []
 
-        if not digits:
-            return result
+        if not digits:                              # O(1)
+            return result                           # O(1)
 
-        letters = {
+        letters = {                                 # O(1)
             "2": "abc",
             "3": "def",
             "4": "ghi",
@@ -17,16 +17,14 @@ class Solution:
         }
 
         def backtrack(index, cur):
-            if len(cur) == len(digits):
-                result.append("".join(cur[:]))
+            if len(cur) == len(digits):             # O(1)
+                result.append("".join(cur[:]))      # O(k) - Convert list to string (k = len of cur)
                 return
+            
+            for ch in letters[digits[index]]:       # O(1) - Access dict, O(4) max 4 letters per digit)
+                cur.append(ch)                      # O(1) - Add character to list
+                backtrack(index + 1, cur)           # Recursive call
+                cur.pop()                           # O(1) - Backtracking step
 
-            comb_letter = letters[digits[index]]
-
-            for ch in comb_letter:
-                cur.append(ch)
-                backtrack(index + 1, cur)
-                cur.pop()
-
-        backtrack(0, [])
-        return result
+        backtrack(0, [])                            # Start recursion
+        return result                               # O(1)
