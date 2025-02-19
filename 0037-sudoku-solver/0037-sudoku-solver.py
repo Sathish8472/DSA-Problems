e5c60d567689b0d8e2fa90c3b9df57429e4ed298
Sathish <sathish8472@gmail.com>
2025-02-19 18:11:20 +0000

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0037-sudoku-solver/0037-sudoku-solver.py
+++ 0037-sudoku-solver/0037-sudoku-solver.py
@@ -1,5 +1,51 @@
 class Solution:
     def solveSudoku(self, board: List[List[str]]) -> None:
+        rows = [set() for _ in range(9)]
+        cols = [set() for _ in range(9)]
+        boxes = [set() for _ in range(9)]
+
+        for r in range(9):
+            for c in range(9):
+                if board[r][c] != ".":
+                    num = board[r][c]
+                    rows[r].add(num)
+                    cols[c].add(num)
+                    boxes[(r // 3) * 3 + (c // 3)].add(num)
+
+        def is_valid(board, row, col, num):
+            return (
+                num not in rows[row]
+                and num not in cols[col]
+                and num not in boxes[(row // 3) * 3 + (col // 3)]
+            )
+
+        def backtrack():
+            for r in range(9):
+                for c in range(9):
+                    if board[r][c] == ".":
+                        for num in map(str, range(1, 10)):
+                            if is_valid(board, r, c, num):
+                                board[r][c] = num
+                                rows[r].add(num)
+                                cols[c].add(num)
+                                boxes[(r // 3) * 3 + (c // 3)].add(num)
+
+                                if backtrack():
+                                    return True
+                                board[r][c] = "."
+                                if num in rows[r]:
+                                    rows[r].remove(num)
+                                if num in cols[c]:
+                                    cols[c].remove(num)
+                                if num in boxes[(r // 3) * 3 + (c // 3)]:
+                                   boxes[(r // 3) * 3 + (c // 3)].remove(num)
+
+                        return False
+            return True
+
+        backtrack()
+
+    def solveSudoku_1(self, board: List[List[str]]) -> None:
 
         def isValid(board, row, col, num):
             for i in range(9):
