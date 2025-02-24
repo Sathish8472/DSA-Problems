9e6631f0ec17f37f143fbc24e93899a8b8921a05
Sathish <sathish8472@gmail.com>
2025-02-24 19:01:08 +0000

    Time: 0 ms (100%), Space: 18 MB (11.62%) - LeetHub

--- 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.py
+++ 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.py
@@ -11,15 +11,26 @@ class Solution:
             return []
 
         result = []
-        stack = deque([root])
+        stack = [root]
         while stack:
             node = stack.pop()
             result.append(node.val)
 
+            # Push right first so left is processed first
             if node.right:
                 stack.append(node.right)
             if node.left:
                 stack.append(node.left)
 
         return result
-        
+
+
+    
+    # Recursive Solution (Simple but Not Optimal)
+    # Time: O(n) (since we visit each node once)
+    # Space: O(n) (because of recursion stack)
+    def preorderTraversal1(self, root: Optional[TreeNode]) -> List[int]:
+        if not root:
+            return []
+
+        return [root.val] + self.preorderTraversal(root.left) + self.preorderTraversal(root.right)
