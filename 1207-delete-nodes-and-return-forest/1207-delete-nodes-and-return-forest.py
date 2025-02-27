1f354c4cf0acc8fe43aff76ee545965bbe4787f4
Sathish <sathish8472@gmail.com>
2025-02-27 07:59:32 +0000

    Time: 36 ms (100%), Space: 18.3 MB (40.41%) - LeetHub

--- 1207-delete-nodes-and-return-forest/1207-delete-nodes-and-return-forest.py
+++ 1207-delete-nodes-and-return-forest/1207-delete-nodes-and-return-forest.py
@@ -5,6 +5,39 @@
 #         self.left = left
 #         self.right = right
 class Solution:
+
+    # Iteration (BFS)
+    def delNodes(self, root: Optional[TreeNode], to_delete: List[int]) -> List[TreeNode]:
+
+        if not root:
+            return []
+        
+        to_delete_set = set(to_delete)
+        forest = []
+        queue = deque([root, False])
+
+        while queue:
+            node, has_parent = queue.popleft()
+            root_deleted = node.val in to_delete_set
+
+            if not root_deleted and not has_parent:
+                forest.append(node)
+            
+            if node.left:
+                queue.append((node.left, not root_deleted))
+                if node.left.val in to_delete_set:
+                    node.left = None
+                
+            if node.right:
+                queue.append((node.right, not root_deleted))
+                if node.right.val in to_delete_set:
+                    node.right = None
+
+        return forest
+
+
+    # Recursion (DFS)
+    # Time: O(N), SpacE: O(N)
     def delNodes(self, root: Optional[TreeNode], to_delete: List[int]) -> List[TreeNode]:
 
         to_delete_set = set(to_delete)
