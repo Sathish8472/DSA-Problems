dcc2d7fa336c49373774291b257758157deb6300
Sathish <sathish8472@gmail.com>
2025-03-02 07:48:53 +0000

    Time: 0 ms (100%), Space: 17.7 MB (52.66%) - LeetHub

--- 0027-remove-element/0027-remove-element.py
+++ 0027-remove-element/0027-remove-element.py
@@ -1,23 +1,12 @@
 class Solution:
-
-    # Two pointer technique(Efficient way)
     def removeElement(self, nums: List[int], val: int) -> int:
-        count = 0
+        
+        k = val
+        pointer = 0
+        for i in range(len(nums)):
+            if nums[i] != k:
+                nums[pointer] = nums[i]
+                pointer += 1
 
-        for num in nums:
-            if num != val:
-                nums[count] = num
-                count += 1
-
-        return count
-
-    # Brute force
-    # Additional space for New Array
-    def removeElement_1(self, nums: List[int], val: int) -> int:
-        new_nums = [num for num in nums if num != val]
-
-        # change the values in nums
-        for i in range(len(new_nums)):
-            nums[i] = new_nums[i]
-
-        return len(new_nums)
+        print(nums)
+        return pointer 
