18a1e5175479c77a5a86d6000aa28fd9e97abd19
Sathish <sathish8472@gmail.com>
2025-03-02 07:14:21 +0000

    Time: 4 ms (70.97%), Space: 19.3 MB (87.3%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -1,43 +1,17 @@
 class Solution:
-    # Boyer-Moore Voting Algorithm
-    # time: O(N), Space: O(1)
     def majorityElement(self, nums: List[int]) -> int:
+        candidate = nums[0]
         count = 0
-        candidate = None
 
-        for num in nums:
+        for i in range(len(nums)):
+            if nums[i] == candidate:
+                count += 1
+            else:
+                count -= 1
+            
             if count == 0:
-                candidate = num
-
-            # Increment count if the current number is candidate, else decrement it
-            count += (1 if candidate == num else -1)
-
+                candidate = nums[i]
+                count += 1
+            
         return candidate
-
-
-
-
-    # Sorting
-    def majorityElement_3(self, nums: List[int]) -> int:
-        nums.sort()             # cost n logn 
-        return nums[len(nums) // 2]
-
-
-
-    # HashMap with built-in
-    # Time: O(N), Space: O(N)
-    def majorityElement_2(self, nums: List[int]) -> int:
-        counts = collections.Counter(nums)
-        print(counts)
-        return max(counts.keys(), key = counts.get)
-
-
-
-    # Brute force, Time: O(N ^ 3)
-    def majorityElement_1(self, nums: List[int]) -> int:
-        majority_count = len(nums) // 2
         
-        for num in nums:
-            count = sum(1 for element in nums if element == num) # takes O(N) for each iteration
-            if count > majority_count:
-                return num
