96ba9206b9e5ccce839818678fe502e41ee92560
Sathish <sathish8472@gmail.com>
2025-03-03 05:03:54 +0000

    Time: 26 ms (80.46%), Space: 35.6 MB (5.26%) - LeetHub

--- 2265-partition-array-according-to-given-pivot/2265-partition-array-according-to-given-pivot.py
+++ 2265-partition-array-according-to-given-pivot/2265-partition-array-according-to-given-pivot.py
@@ -1,45 +1,7 @@
 class Solution:
 
-    def pivotArray(self, nums: List[int], pivot: int) -> List[int]:
-        ans = [0] * len(nums)
-        less_i = 0
-        greater_i = len(nums) - 1
-
-        for i, j in zip(range(len(nums)), range(len(nums) - 1, -1, -1)):
-            if nums[i] < pivot:
-                ans[less_i] = nums[i]
-                less_i += 1
-            if nums[j] > pivot:
-                ans[greater_i] = nums[j]
-                greater_i -= 1
-
-        while less_i <= greater_i:
-            ans[less_i] = pivot
-            less_i += 1
-
-        return ans
-
-    # Optimal: In-Place Dutch National Flag
-    def pivotArray_4(self, nums: List[int], pivot: int) -> List[int]:
-        arr = nums[:]
-        n = len(nums)
-        low, high = 0, n - 1
-
-        for i in range(n):
-            if arr[i] < pivot:
-                arr[i], arr[low] = arr[low], arr[i]
-                low += 1
-            
-        for i in range(n - 1, -1, -1):
-            if arr[i] > pivot:
-                arr[i], arr[high] = arr[high], arr[i]
-                high -= 1
-            
-        return arr
-
-
     # Dutch National Flag Variation (Two Pointers)
-    def pivotArray_3(self, nums: list[int], pivot: int) -> List[int]:
+    def pivotArray(self, nums: list[int], pivot: int) -> List[int]:
         left, right = [], []
         count = 0  # Count occurrences of pivot
 
