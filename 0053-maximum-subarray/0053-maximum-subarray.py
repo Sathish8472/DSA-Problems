5770209b8a92b692e44415213b0868d45f8eb115
Sathish <sathish8472@gmail.com>
2025-03-05 09:11:48 +0000

    Time: 86 ms (38.37%), Space: 32.1 MB (65.75%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.py
+++ 0053-maximum-subarray/0053-maximum-subarray.py
@@ -1,34 +1,36 @@
 class Solution:
 
+    # Optimal
     def maxSubArray(self, nums: List[int]) -> int:
         max_sum = float('-inf')
         current_sum = float('-inf')
 
-        for i in range(len(nums)):
-            current_sum = max(nums[i], current_sum + nums[i])
+        for num in nums:
+            current_sum = max(num, current_sum + num)
             max_sum = max(max_sum, current_sum)
 
         return max_sum
+    
+
 
     # backtrack, Not Efficient
     # Time: O(N ^ 2), Space: O(N)
-    def maxSubArray_1(self, nums: List[int]) -> int:
-        maxSum = float('-inf')
+    def maxSubArray1(self, nums: List[int]) -> int:
+        max_sum = float('-inf')
 
         def backtrack(start, end):
-            nonlocal maxSum
+            nonlocal max_sum
 
             if end > len(nums):
-                return
+                return 
             
             current_subarray = nums[start:end]
             current_sum = sum(current_subarray)
-            maxSum = max(maxSum, current_sum)
-            
+            max_sum = max(max_sum, current_sum)
+
             backtrack(start, end + 1)
 
         for i in range(len(nums)):
             backtrack(i, i + 1)
 
-        return maxSum
-        
+        return max_sum  
