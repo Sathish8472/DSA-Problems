b24edf9dcd4f1dec5ae78b505e8d79b6e65abee1
Sathish <sathish8472@gmail.com>
2025-03-05 08:49:18 +0000

    Time: 0 ms (100%), Space: 17.6 MB (75.19%) - LeetHub

--- 2649-count-total-number-of-colored-cells/2649-count-total-number-of-colored-cells.py
+++ 2649-count-total-number-of-colored-cells/2649-count-total-number-of-colored-cells.py
@@ -1,5 +1,13 @@
 class Solution:
+
+    # Optimal
+    # Time: O(1), Space: O(1)
     def coloredCells(self, n: int) -> int:
+        return 2 * n * (n - 1) + 1
+
+    # Not Efficient
+    # time: O(N)
+    def coloredCells1(self, n: int) -> int:
 
         def getCombination(n1):
             if n1 == 1:
@@ -8,4 +16,3 @@ class Solution:
             return (n1 - 1) * 4 + getCombination(n1 - 1)
 
         return getCombination(n)
-        
