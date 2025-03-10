d94ab3eb7c2910f71a1649d8d1f1897240f799a8
Sathish <sathish8472@gmail.com>
2025-03-10 19:02:27 +0000

    Time: 11 ms (19.01%), Space: 19.8 MB (14.26%) - LeetHub

--- 0136-single-number/0136-single-number.py
+++ 0136-single-number/0136-single-number.py
@@ -1,9 +1,27 @@
 class Solution:
+
+    # Hashtable
+    # Time: O(N), Space: O(N)
     def singleNumber(self, nums: List[int]) -> int:
+        hashtable = defaultdict(int)
+
+        for num in nums:
+            hashtable[num] += 1
+        
+        for num in hashtable:
+            if hashtable[num] == 1:
+                return num
+            
+        return 0
+
+
+    # Brute List operation
+    # time: O(N * N), SpacE: O(N)
+    def singleNumber_1(self, nums: List[int]) -> int:
         no_duplicate_list = []
 
         for num in nums:
-            if num not in no_duplicate_list:
+            if num not in no_duplicate_list:            # time: O(N)
                 no_duplicate_list.append(num)
             else:
                 no_duplicate_list.remove(num)
