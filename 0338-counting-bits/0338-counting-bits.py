141607c509fe2da319263915aeb828284838353f
Sathish <sathish8472@gmail.com>
2025-03-10 19:42:21 +0000

    Time: 3 ms (94.54%), Space: 18.6 MB (22.98%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.py
+++ 0338-counting-bits/0338-counting-bits.py
@@ -1,5 +1,18 @@
 class Solution:
-    def countBits(self, n: int) -> List[int]:
+
+    def countBits(self, n : int) -> List[int]:
+        ans = [0] * (n + 1)
+
+        for i in range(1, n + 1):
+            ans[i] = ans[i >> 1] + (i & 1)  # Right shift + last bit check
+        
+        return ans
+
+        
+
+    # Brute
+    # time: O(n log n), Space: O(N)
+    def countBits_1(self, n: int) -> List[int]:
         ans = [0] * (n + 1)
 
         def getCount(num):
@@ -13,5 +26,4 @@ class Solution:
             count = getCount(i)
             ans[i] = count
 
-        print(ans)
         return ans
