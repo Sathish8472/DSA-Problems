d0d3a5e302571efc0d07dedb74adf53a7af3b9f1
Sathish <sathish8472@gmail.com>
2025-03-12 08:00:08 +0000

    Time: 5 ms (5.12%), Space: 18.1 MB (41.77%) - LeetHub

--- 2614-maximum-count-of-positive-integer-and-negative-integer/2614-maximum-count-of-positive-integer-and-negative-integer.py
+++ 2614-maximum-count-of-positive-integer-and-negative-integer/2614-maximum-count-of-positive-integer-and-negative-integer.py
@@ -1,39 +1,35 @@
 class Solution:
 
-    def maximumCount1(self, nums: List[int]) -> int:
+    def maximumCount(self, nums: List[int]) -> int:
         if not nums:
             return 0
         
         left, right = 0, len(nums) - 1
         neg, pos = 0, 0
 
-        while left < right:
+        while left <= right:
             if nums[left] < 0:
                 neg += 1
-            if nums[left] > 0:
+            elif nums[left] > 0:
                 pos += 1
 
-            if nums[right] < 0:
-                neg += 1
-            if nums[right] > 0:
-                pos += 1
+            if left != right:
+                if nums[right] < 0:
+                    neg += 1
+                if nums[right] > 0:
+                    pos += 1
 
             left += 1
             right -= 1
 
-        if left == right:
-            if nums[left] < 0:
-                neg += 1
-            if nums[left] > 0:
-                pos += 1
-
-        print(left, right)
         print(f"pos: {pos}, neg: {neg}")
         return max(pos, neg)
 
-    def maximumCount(self, nums: List[int]) -> int:
-        pos_count = 0
-        neg_count = 0
+    def maximumCount1(self, nums: List[int]) -> int:
+        if not nums:
+            return 0
+        
+        pos_count, neg_count = 0, 0
 
         for num in nums:
             if num < 0:
