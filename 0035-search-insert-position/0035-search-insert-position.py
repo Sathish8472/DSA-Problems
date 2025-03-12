ce1cfe6701980359a3c6fa65b765a77f2c77aa8f
Sathish <sathish8472@gmail.com>
2025-03-12 09:26:51 +0000

    Time: 0 ms (100%), Space: 18.6 MB (5.15%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.py
+++ 0035-search-insert-position/0035-search-insert-position.py
@@ -1,8 +1,25 @@
 class Solution:
+
+    # Binary Search implementation
+    # time: O(Log N)
     def searchInsert(self, nums: List[int], target: int) -> int:
-        
+
+        left, right = 0, len(nums)
+        while left < right:
+            mid = (left + right) // 2
+
+            if nums[mid] < target:
+                left = mid + 1
+            else:
+                right = mid
+
+        return left
+
+
+    # Using Bisect
+    def searchInsert1(self, nums: List[int], target: int) -> int:
+
         index = bisect.bisect_left(nums, target)
         print(index)
 
         return index
-        
