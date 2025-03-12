aaeff922c01074478f5b677ec65a59df0a131b9d
Sathish <sathish8472@gmail.com>
2025-03-12 19:16:45 +0000

    Time: 0 ms (100%), Space: 17.9 MB (38.84%) - LeetHub

--- 1537-maximum-score-after-splitting-a-string/1537-maximum-score-after-splitting-a-string.py
+++ 1537-maximum-score-after-splitting-a-string/1537-maximum-score-after-splitting-a-string.py
@@ -1,40 +1,17 @@
 class Solution:
-    
+
     def maxScore(self, s: str) -> int:
         max_score = 0
         zero_score = 0
-        ones_score = s.count('1')
-
-        print(f"Ones: {ones_score}")
+        ones_score = s.count("1")  # Count total number of 1s in the string
 
         for i in range(len(s) - 1):
-            if s[i] == '0':
+            if s[i] == "0":
                 zero_score += 1
             else:
                 ones_score -= 1
-            
-            print(f"i: {i}, Ones: {ones_score}, zeros: {zero_score}")
+
             max_score = max(max_score, zero_score + ones_score)
-            print(f"max: {max_score}")
-        
-        print(f"Ones: {ones_score}")
-        print(f"Zero: {zero_score}")
-
-        return max_score
-
-
-    def maxScore2(self, s: str) -> int:
-        total_ones = s.count("1")  # Count total number of 1s in the string
-        left_zeros = 0
-        max_score = 0
-
-        for i in range(len(s) - 1):  # Split at index i (not including last character)
-            if s[i] == "0":
-                left_zeros += 1
-            else:
-                total_ones -= 1  # Removing 1 from right part
-            
-            max_score = max(max_score, left_zeros + total_ones)
 
         return max_score
 
