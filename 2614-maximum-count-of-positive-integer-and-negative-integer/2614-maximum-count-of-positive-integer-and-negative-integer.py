8da327cbbe7816422b5c42a84ec9d8fa57c392a2
Sathish <sathish8472@gmail.com>
2025-03-12 08:13:01 +0000

    Time: 0 ms (100%), Space: 18.2 MB (22.96%) - LeetHub

--- 2614-maximum-count-of-positive-integer-and-negative-integer/2614-maximum-count-of-positive-integer-and-negative-integer.py
+++ 2614-maximum-count-of-positive-integer-and-negative-integer/2614-maximum-count-of-positive-integer-and-negative-integer.py
@@ -1,9 +1,26 @@
 class Solution:
 
+
     def maximumCount(self, nums: List[int]) -> int:
         if not nums:
             return 0
-        
+
+        neg_count = bisect.bisect_left(nums, 0)
+        pos_count = bisect.bisect_right(nums, 0)
+
+        pos_count = len(nums) - pos_count
+        print(f"pos: {pos_count}, neg: {neg_count}")
+        return max(pos_count, neg_count)
+
+
+
+    # Two pointer technique
+    # time: O(N), Space: O(1)
+    # This technique takes time O(n / 2), but in Big If Notation its will be O(N)
+    def maximumCount1(self, nums: List[int]) -> int:
+        if not nums:
+            return 0
+
         left, right = 0, len(nums) - 1
         neg, pos = 0, 0
 
@@ -28,7 +45,7 @@ class Solution:
     def maximumCount1(self, nums: List[int]) -> int:
         if not nums:
             return 0
-        
+
         pos_count, neg_count = 0, 0
 
         for num in nums:
@@ -39,9 +56,9 @@ class Solution:
 
         print(f"pos: {pos_count}, neg: {neg_count}")
         return max(pos_count, neg_count)
-    
+
     def maximumCount_1(self, nums: List[int]) -> int:
-        
+
         pos_count = sum(1 for num in nums if num > 0)
         neg_count = sum(1 for num in nums if num < 0)
 
