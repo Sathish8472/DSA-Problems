8a5bc7f844fadffe54f8f34b62677ff8a5b11760
Sathish <sathish8472@gmail.com>
2025-03-12 07:50:35 +0000

    Time: 4 ms (12.81%), Space: 18.2 MB (22.96%) - LeetHub

--- 2614-maximum-count-of-positive-integer-and-negative-integer/2614-maximum-count-of-positive-integer-and-negative-integer.py
+++ 2614-maximum-count-of-positive-integer-and-negative-integer/2614-maximum-count-of-positive-integer-and-negative-integer.py
@@ -1,6 +1,37 @@
 class Solution:
+
     def maximumCount(self, nums: List[int]) -> int:
-        n = len(nums)
+        if not nums:
+            return 0
+        
+        left, right = 0, len(nums) - 1
+        neg, pos = 0, 0
+
+        while left < right:
+            if nums[left] < 0:
+                neg += 1
+            if nums[left] > 0:
+                pos += 1
+
+            if nums[right] < 0:
+                neg += 1
+            if nums[right] > 0:
+                pos += 1
+
+            left += 1
+            right -= 1
+
+        if left == right:
+            if nums[left] < 0:
+                neg += 1
+            if nums[left] > 0:
+                pos += 1
+
+        print(left, right)
+        print(f"pos: {pos}, neg: {neg}")
+        return max(pos, neg)
+
+    def maximumCount1(self, nums: List[int]) -> int:
         pos_count = 0
         neg_count = 0
 
@@ -9,8 +40,6 @@ class Solution:
                 neg_count += 1
             elif num > 0:
                 pos_count += 1
-        
-        print(f'pos: {pos_count}, neg: {neg_count}')
-        return max(pos_count, neg_count)
 
-        
+        print(f"pos: {pos_count}, neg: {neg_count}")
+        return max(pos_count, neg_count)
