21963b3ab3844db3df581aec12df5aebe8d4577e
Sathish <sathish8472@gmail.com>
2025-03-19 08:26:59 +0000

    Time: 484 ms (94.43%), Space: 0B (100%) - LeetHub

--- 0197-rising-temperature/0197-rising-temperature.sql
+++ 0197-rising-temperature/0197-rising-temperature.sql
@@ -1,5 +1,18 @@
 # Write your MySQL query statement below
+
+
 SELECT W1.id
-FROM 
-    Weather W1, Weather W2
-Where dateDiff(W1.recordDate, W2.recordDate) = 1 And W1.temperature > W2.temperature
+From
+    Weather W1
+JOIN
+    Weather W2
+ON 
+    dateDiff(W1.recordDate, W2.recordDate) = 1
+WHERE
+    W1.temperature > W2.temperature
+
+-- Approach
+-- SELECT W1.id
+-- FROM 
+--     Weather W1, Weather W2
+-- Where dateDiff(W1.recordDate, W2.recordDate) = 1 And W1.temperature > W2.temperature
