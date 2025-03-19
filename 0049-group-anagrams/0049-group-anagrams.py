596d5f1695b1279a2fe729cf92321e80c67d29e3
Sathish <sathish8472@gmail.com>
2025-03-19 18:45:58 +0000

    Time: 20 ms (27.29%), Space: 21.2 MB (44.29%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,31 +1,21 @@
 class Solution:
-
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        freq_map = defaultdict(list)    # Time: O(1), space: O(1)
+        words = set()
 
-        for s in strs:                           # Time: O(n), space: O(1)
-            count = [0] * 26                     # Time: O(k), space: O(26)
-            for ch in s:                         # Time: O(k), space: O(1)
-                count[ord(ch) - ord('a')] += 1   # Time: O(1), space: O(1)
-            
-            # Use the tuple as the key
-            freq_map[tuple(count)].append(s)  # Time: O(1) average for tuple operations, space: O(1)
-        return list(freq_map.values())          # Time: O(n), space: O(n * k)
+        for word in strs:
+            words.add("".join(sorted(word)))
 
+        hashmap = {}
+        for word in words:
+            hashmap[word] = []
 
+        result = []
 
-    # Brute force
-    # Time : O(n * k log k), Space: O(n * k)
-    def groupAnagrams2(self, strs: List[str]) -> List[List[str]]:
-        
-        anagram_map : Dict[str, List[str]] = {}    
+        for word in strs:
+            sorted_word = "".join(sorted(word))
+            hashmap[sorted_word].append(word)
 
-        for word in strs:                                # Time: O(len(word)), space: O(1)
-            sorted_word = "".join(sorted(word))             # Time: O(k log k), space: O(k)
-            if sorted_word not in anagram_map:
-                anagram_map[sorted_word] = []
+        for value in hashmap.values():
+            result.append(value)
 
-            anagram_map[sorted_word].append(word)       # Time: O(1), space: O(1)
-
-        return [group for group in anagram_map.values()]    # Time: O(n), space: O(n * k)
-        
+        return result
