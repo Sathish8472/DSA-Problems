108c9418083b7dfd3680b89209540397a118bc6b
Sathish <sathish8472@gmail.com>
2025-03-21 18:30:20 +0000

    Time: 71 ms (61.26%), Space: 19.7 MB (79.98%) - LeetHub

---
+++ 2220-find-all-possible-recipes-from-given-supplies/2220-find-all-possible-recipes-from-given-supplies.py
@@ -0,0 +1,55 @@
+class Solution:
+    def findAllRecipes(self, recipes: List[str], ingredients: List[List[str]], supplies: List[str]) -> List[str]:
+        remaining_ingredients = {}
+        needed_for = {}
+
+        for recipe, ingredient_list in zip(recipes, ingredients):
+            remaining_ingredients[recipe] = len(ingredient_list)
+
+            for ing in ingredient_list:
+                if ing not in needed_for:
+                    needed_for[ing] = []
+                needed_for[ing].append(recipe)
+            
+        available = deque(supplies)
+        result = []
+
+        while available:
+            item = available.popleft()
+
+            if item in needed_for:
+                for recipe in needed_for[item]:
+                    remaining_ingredients[recipe] -= 1
+                    if remaining_ingredients[recipe] == 0:
+                        result.append(recipe)
+                        available.append(recipe)
+                    
+                
+            
+        return result
+
+
+    def findAllRecipes1(self, recipes: List[str], ingredients: List[List[str]], supplies: List[str]) -> List[str]:
+        
+        result = []
+        haspmap = {}
+
+        for i in range(len(recipes)):
+            
+            recipe = recipes[i]
+            ingredient = ingredients[i]
+          
+            isAllIng = True
+
+            for ing in ingredient:
+                if ing not in supplies:
+                    print("in: ",ingredient )
+                    isAllIng = False;
+
+            if isAllIng:
+                result.append(recipe)
+                supplies.append(recipe)
+
+
+        print("result:", result)
+        return result
