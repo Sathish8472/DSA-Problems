1ef3729e2a031cce473ad0774b7bf99408774845
Sathish <sathish8472@gmail.com>
2025-03-24 13:25:15 +0000

    Time: 2 ms (84.28%), Space: 17.8 MB (40.42%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.py
+++ 0013-roman-to-integer/0013-roman-to-integer.py
@@ -1,24 +1,19 @@
 class Solution:
     def romanToInt(self, s: str) -> int:
-        hashmap = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
+        roman_map = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
 
-        result = 0
-        prev_val = float('inf')
-        curr_val = 0
+        total = 0
+        prev_val = 0
+
         for ch in s:
-            curr_val = hashmap[ch]
+            curr_val = roman_map[ch]
 
-            print("curr_Val: ", curr_val)
             if curr_val > prev_val:
-                
-                result -= prev_val
-                result += curr_val - prev_val
-                print("inside result: ", result)
+                total -= prev_val
+                total += curr_val - prev_val
             else:
-                result += curr_val
+                total += curr_val
+
             prev_val = curr_val
-            print("result: ", result)
 
-        return result
-
-        
+        return total
