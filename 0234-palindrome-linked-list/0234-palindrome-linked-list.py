506f3b756c3b8adde7d4b676184236dc39ceebe9
Sathish <sathish8472@gmail.com>
2025-03-25 16:24:58 +0000

    Time: 39 ms (41.37%), Space: 35 MB (52.64%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.py
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.py
@@ -4,10 +4,50 @@
 #         self.val = val
 #         self.next = next
 class Solution:
+
+    # Optimal Solution
+    # Time: O(N), Space: O(1)
     def isPalindrome(self, head: Optional[ListNode]) -> bool:
+        if not head or not head.next:
+            return True
+        
+        slow, fast = head, head
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+        
+        second_half_head = self.reverse(slow)
 
+        first_half, second_half = head, second_half_head
+
+        while second_half:
+            if first_half.val != second_half.val:
+                return False
+            
+            first_half = first_half.next
+            second_half = second_half.next
+        
+        self.reverse(second_half_head)
+
+        return True
+
+    def reverse(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        prev = None
+        current = head
+        
+        while current:
+            front = current.next
+            current.next = prev
+            prev = current
+            current = front
+        
+        return prev
+
+    # Brute
+    # Time: O(N), SpacE: O(N)
+
+    def isPalindrome_1(self, head: Optional[ListNode]) -> bool:
         stack = []
-
         temp = head
 
         while temp:
