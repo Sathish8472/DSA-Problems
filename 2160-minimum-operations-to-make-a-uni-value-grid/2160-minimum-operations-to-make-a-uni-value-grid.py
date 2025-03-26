8b7abfb7c46ec1935c7fa044f48a041ee9a3b326
Sathish <sathish8472@gmail.com>
2025-03-26 08:06:52 +0000

    Time: 137 ms (84.65%), Space: 39.1 MB (50.44%) - LeetHub

--- 2160-minimum-operations-to-make-a-uni-value-grid/2160-minimum-operations-to-make-a-uni-value-grid.py
+++ 2160-minimum-operations-to-make-a-uni-value-grid/2160-minimum-operations-to-make-a-uni-value-grid.py
@@ -2,50 +2,17 @@ class Solution:
     def minOperations(self, grid: List[List[int]], x: int) -> int:
         m = grid[0]
         n = grid[0][0]
-        arr = []
+        arr = [num for row in grid for num in row]
 
-        isElementEven = True
-        for gd in grid:
-            for g in gd:
-                if g % 2 != 0:
-                    isElementEven = False
-                arr.append(g)
-            
+        remainder = arr[0] % x
+        if any(num % x != remainder for num in arr):
+            return -1
+
         arr.sort()
-        middle_element = arr[len(arr) // 2]
+        median = arr[len(arr) // 2]
         
-        # isEven = False
-        # if x % 2 == 0:
-        #     isEven = True
-        # else:
-        #     isEven = False
-        
-        # if isEven and isElementEven:
-        #     print("eve Proceed")
-        # elif not isEven and not isElementEven:
-        #     print("od proceed")
-        # else:
-        #     print("not proceed")
-        #     return -1
-
-        # print(middle_element)
-        # print(arr)
-
         operation_count = 0
         for num in arr:
-            if num % x != middle_element % x:
-                return -1
-            
-            operation_count += abs(middle_element - num) // x
-
-            # while num != middle_element:
-            #     if num > middle_element:
-            #         num -= x
-            #         operation_count += 1
-            #     else:
-            #         num += x
-            #         operation_count += 1
-
-        print(operation_count)
+            operation_count += abs(median - num) // x
 
         return operation_count
