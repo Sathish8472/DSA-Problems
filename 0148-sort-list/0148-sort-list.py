f11467125bbb1d48d7f2414e0016961507bfe8f5
Sathish <sathish8472@gmail.com>
2025-03-26 19:37:43 +0000

    Time: 16 ms (95.89%), Space: 32.7 MB (87.84%) - LeetHub

--- 0148-sort-list/0148-sort-list.py
+++ 0148-sort-list/0148-sort-list.py
@@ -4,6 +4,60 @@
 #         self.val = val
 #         self.next = next
 class Solution:
+
+    # USing Merge sort
+    def sortList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        if not head or not head.next:
+            return head
+        
+        middle = self.findMiddle(head)
+
+        right = middle.next
+        middle.next = None
+        left = head
+
+        left = self.sortList(left)
+        right = self.sortList(right)
+
+        return self.mergeTwoSortedLinkedList(left, right)
+
+    def mergeTwoSortedLinkedList(self, list1, list2):
+        dummyNode = ListNode(-1)
+        temp = dummyNode
+
+        while list1 and list1:
+            if list1.val <= list2.val:
+                temp.next = list1
+                list1 = list1.next
+            else:
+                temp.next = list2
+                list2 = list2.next
+            
+            temp = temp.next
+        
+        if list1:
+            temp.next = list1
+        else:
+            temp.next = list2
+        
+        return dummyNode.next
+
+
+    def findMiddle(self, head):
+        if not head or not head.next:
+            return head
+        
+        slow = head
+        fast = fast.next
+
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+        
+        return slow
+
+
+    # Brute
     def sortList(self, head: Optional[ListNode]) -> Optional[ListNode]:
         arr = []
         temp = head
