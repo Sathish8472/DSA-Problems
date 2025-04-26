37819e29031fed55ddae5996f4ff185ba9a8fc87
Sathish <sathish8472@gmail.com>
2025-04-26 12:57:20 +0100

    Time: 0 ms (100%), Space: 18.2 MB (14.91%) - LeetHub

--- 0046-permutations/0046-permutations.py
+++ 0046-permutations/0046-permutations.py
@@ -1,47 +1,23 @@
 class Solution:
-
-    # Optmized code
-    # Time: O(N!), Space: O(N)
     def permute(self, nums: List[int]) -> List[List[int]]:
-        result = []
 
-        def backtrack(cur_perm):
-            if len(cur_perm) == len(nums):
-                result.append(cur_perm[:])
+        result = []
+        n = len(nums)
+
+        def backtrack(current_subset):
+            if len(current_subset) == n:
+                result.append(current_subset[:])
                 return
 
             for num in nums:
-                if num in cur_perm:
+                if num in current_subset:
                     continue
-                cur_perm.append(num)
-                backtrack(cur_perm)
-                cur_perm.pop()
+
+                current_subset.append(num)
+                backtrack(current_subset)
+                current_subset.pop()
 
         backtrack([])
+
         return result
-
-
-
-
-    def permute1(self, nums: List[int]) -> List[List[int]]:
-        result = []
-        ds = []
-        freq = [False] * len(nums)
-
-        def generatePermute():
-            if len(ds) == len(nums):
-                result.append(
-                    ds[:]
-                )  # Adds a copy of the current permutation to result (to avoid reference issues).
-                return
-
-            for i, num in enumerate(nums):
-                if not freq[i]:
-                    freq[i] = True
-                    ds.append(num)
-                    generatePermute()
-                    freq[i] = False
-                    ds.pop()
-
-        generatePermute()
-        return result
+        
