a4810dc0d6512503baf5c3b6a278110b56577f88
Sathish <sathish8472@gmail.com>
2025-04-27 10:42:05 +0100

    Time: 7 ms (57.8%), Space: 18 MB (77.71%) - LeetHub

--- 0047-permutations-ii/0047-permutations-ii.py
+++ 0047-permutations-ii/0047-permutations-ii.py
@@ -1,8 +1,37 @@
 class Solution:
 
+    def permuteUnique(self, nums: List[int]) -> List[List[int]]:
+        n = len(nums)
+        result = []
+        nums.sort()
+        print(nums)
+        visited = [False] * n
+
+        def backtrack(current_subset):
+            nonlocal result
+
+            if len(current_subset) == n:
+                result.append(current_subset[:])
+                return 
+
+            for i in range(n):
+                if i > 0 and nums[i] == nums[i - 1] and not visited[i - 1]:
+                    continue
+
+                if not visited[i]:
+                    current_subset.append(nums[i])
+                    visited[i] = True
+                    backtrack(current_subset)
+                    current_subset.pop()
+                    visited[i] = False
+
+
+        backtrack([])
+        return result
+
     # Backtracking Approach
     # Time: O(n! + n log n), Space: O(N!)
-    def permuteUnique(self, nums: List[int]) -> List[List[int]]:
+    def permuteUnique1(self, nums: List[int]) -> List[List[int]]:
         result = []
         nums.sort()  # O(N Log N)
         visited = [False] * len(nums)
