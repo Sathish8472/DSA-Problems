c2298e3b20acdc618489d953629323dff8b22e90
Sathish <sathish8472@gmail.com>
2025-04-30 18:33:46 +0100

    Time: 10 ms (62.28%), Space: 18.1 MB (20.07%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.py
+++ 0039-combination-sum/0039-combination-sum.py
@@ -1,10 +1,38 @@
 class Solution:
 
+    def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
+        result = []
+        self.func(0, candidates, [], target, result)
+        return result
+
+    def func(self, ind, nums, current_sub, current_sum, result):
+
+        if current_sum < 0:
+            return 
+        
+        if current_sum == 0:
+            result.append(current_sub[:])
+            return 
+        
+        if ind == len(nums):
+            return
+        
+        current_sub.append(nums[ind])
+        current_sum -= nums[ind]
+        
+        self.func(ind, nums, current_sub, current_sum, result)
+        current_sum += nums[ind]
+        current_sub.pop()
+        self.func(ind + 1, nums, current_sub, current_sum, result)
+
+        return
+
+
     # Backtrack
     # Time: O(2 ^ n)
     # Space Complexity: O(T) (due to recursion depth) + O(k) (to store results, where k is the number of combinations)
     
-    def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
+    def combinationSum2(self, candidates: List[int], target: int) -> List[List[int]]:
         result = []
 
         def backtrack(index, current, target):
