1ae81ecec936bc7f2d57c6b706b4b1f47607d7f0
Sathish <sathish8472@gmail.com>
2025-05-04 07:57:02 +0100

    Time: 15 ms (73.67%), Space: 18.3 MB (89.59%) - LeetHub

--- 0254-factor-combinations/0254-factor-combinations.py
+++ 0254-factor-combinations/0254-factor-combinations.py
@@ -1,34 +1,22 @@
 class Solution:
     def getFactors(self, n: int) -> List[List[int]]:
-
-        if n == 1:
-            return []
-
-        nums = []
-        for i in range(2, n):
-            nums.append(i)
-
         result = []
-        self._calculate_factors(0, nums, [], 1, n, result)
+
+        def backtrack(start, current, target):
+            if target == 1 and len(current) > 1:
+                result.append(current[:])
+                return
+
+            for i in range(start, int(target**0.5) + 1):
+                if target % i == 0:
+                    current.append(i)
+                    backtrack(i, current, target // i)
+                    current.pop()
+
+            if target >= start:
+                current.append(target)
+                backtrack(target, current, 1)
+                current.pop()
+
+        backtrack(2, [], n)
         return result
-
-    def _calculate_factors(self, ind, nums, current_sub, factor, target, result):
-        if factor == target:
-            result.append(current_sub[:])
-            return
-
-        if factor > target:
-            return
-
-        if ind >= len(nums):
-            return
-
-        current_sub.append(nums[ind])
-        self._calculate_factors(
-            ind, nums, current_sub, factor * nums[ind], target, result
-        )
-        current_sub.pop()
-
-        self._calculate_factors(ind + 1, nums, current_sub, factor, target, result)
-
-        return
