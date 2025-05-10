7b744fe50955945abeee3729c00a4e5d23e4de35
Sathish <sathish8472@gmail.com>
2025-05-10 11:20:03 +0100

    Time: 0 ms (100%), Space: 18.5 MB (12.62%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.py
+++ 0733-flood-fill/0733-flood-fill.py
@@ -2,12 +2,28 @@ class Solution:
     def floodFill(self, image: List[List[int]], sr: int, sc: int, color: int) -> List[List[int]]:
         row_len = len(image)
         col_len = len(image[0])
-
         visited = [[0] * col_len for _ in range(row_len)]
-
         initial_col = image[sr][sc]
-        
         directions = [(1, 0), (0, 1), (-1, 0), (0, -1)]
+
+        def bfs(row, col):
+            nonlocal visited
+            queue = deque()
+            queue.append((row, col))
+            
+            while queue:
+                r, c = queue.popleft()
+                image[r][c] = color
+                visited[r][c] = 1
+
+                for dr, dc in directions:
+                    nr = r + dr
+                    nc = c + dc
+
+                    if 0 <= nr < row_len and 0 <= nc < col_len and image[nr][nc] == initial_col and visited[nr][nc] != 1:
+                        queue.append((nr, nc))
+
+
 
         def dfs(row, col):
             nonlocal visited
@@ -20,5 +36,5 @@ class Solution:
                 if (0 <= nr < row_len and 0 <= nc < col_len and image[nr][nc] == initial_col and visited[nr][nc] != 1):
                     dfs(nr, nc)
 
-        dfs(sr, sc)
+        bfs(sr, sc)
         return image
