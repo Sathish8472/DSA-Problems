965b686858301539a1508901281aa9462d6b8caf
Sathish <sathish8472@gmail.com>
2025-05-10 12:11:13 +0100

    Time: 87 ms (54.64%), Space: 19.9 MB (92.34%) - LeetHub

--- 1073-number-of-enclaves/1073-number-of-enclaves.py
+++ 1073-number-of-enclaves/1073-number-of-enclaves.py
@@ -5,11 +5,27 @@ class Solution:
 
         visited = [[0] * col_len for _ in range(row_len)]
 
-        directions = [(1, 0), (0, 1), (-1, 0), (0, -1)]
+        for i in range(row_len):
+            for j in range(col_len):
+                if (i == 0 or j == 0 or i == row_len - 1 or j == col_len - 1) and grid[i][j] == 1:
+                    visited[i][j] = 1
+                    self.bfs(i, j, grid, visited, row_len, col_len) 
+                    
+        count = 0
 
-        def bfs(row, col):
+        for i in range(row_len):
+            for j in range(col_len):
+                if grid[i][j] == 1 and visited[i][j] != 1:
+                    count += 1
+
+        return count
+
+    
+    def bfs(self, row, col, grid, visited, row_len, col_len):
             queue = deque()
             queue.append((row, col))
+
+            directions = [(1, 0), (0, 1), (-1, 0), (0, -1)]
 
             while queue:
                 r, c = queue.popleft()
@@ -21,18 +37,3 @@ class Solution:
                     if 0 <= nr < row_len and 0 <= nc < col_len and grid[nr][nc] == 1 and visited[nr][nc] != 1:
                         visited[nr][nc] = 1
                         queue.append((nr, nc))
-
-        for i in range(row_len):
-            for j in range(col_len):
-                if (i == 0 or j == 0 or i == row_len - 1 or j == col_len - 1) and grid[i][j] == 1:
-                    visited[i][j] = 1
-                    bfs(i, j) 
-                    
-        count = 0
-
-        for i in range(row_len):
-            for j in range(col_len):
-                if grid[i][j] == 1 and visited[i][j] != 1:
-                    count += 1
-
-        return count
