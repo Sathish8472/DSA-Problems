ae1ea0e8bf544370562d346677fe090ccddef6c1
Sathish <sathish8472@gmail.com>
2025-05-10 11:38:48 +0100

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.py
+++ 0733-flood-fill/0733-flood-fill.py
@@ -2,39 +2,42 @@ class Solution:
     def floodFill(self, image: List[List[int]], sr: int, sc: int, color: int) -> List[List[int]]:
         row_len = len(image)
         col_len = len(image[0])
-        visited = [[0] * col_len for _ in range(row_len)]
         initial_col = image[sr][sc]
+
         directions = [(1, 0), (0, 1), (-1, 0), (0, -1)]
 
+        result = [row[:] for row in image]
+
         def bfs(row, col):
-            nonlocal visited
+            if result[row][col] == color: 
+                return
+
             queue = deque()
             queue.append((row, col))
             
             while queue:
                 r, c = queue.popleft()
                 image[r][c] = color
-                visited[r][c] = 1
 
                 for dr, dc in directions:
                     nr = r + dr
                     nc = c + dc
 
-                    if 0 <= nr < row_len and 0 <= nc < col_len and image[nr][nc] == initial_col and visited[nr][nc] != 1:
+                    if 0 <= nr < row_len and 0 <= nc < col_len and result[nr][nc] == initial_col:
+                        result[nr][nc] = color
                         queue.append((nr, nc))
 
-
-
         def dfs(row, col):
-            nonlocal visited
-            image[row][col] = color
-            visited[row][col] = 1
+            if result[row][col] == color: 
+                return
+
+            result[row][col] = color
 
             for dr, dc in directions:
                 nr = row + dr
                 nc = col + dc
-                if (0 <= nr < row_len and 0 <= nc < col_len and image[nr][nc] == initial_col and visited[nr][nc] != 1):
+                if 0 <= nr < row_len and 0 <= nc < col_len and result[nr][nc] == initial_col:
                     dfs(nr, nc)
 
         bfs(sr, sc)
-        return image
+        return result
