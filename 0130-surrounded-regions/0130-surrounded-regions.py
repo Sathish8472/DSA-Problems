ec4d6835175ade1459960acd6e9c216633df5cf9
Sathish <sathish8472@gmail.com>
2025-08-05 22:26:49 +0100

    Time: 3 ms (86.95%), Space: 22 MB (30.52%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.py
+++ 0130-surrounded-regions/0130-surrounded-regions.py
@@ -1,33 +1,41 @@
 class Solution:
     def solve(self, board: List[List[str]]) -> None:
-        row_len = len(board)
-        col_len = len(board[0])
+        """
+        Do not return anything, modify board in-place instead.
+        """
 
-        visited = [[0] * col_len for _ in range(row_len)]
-        directions = [(0, 1), (1, 0), (0, -1), (-1, 0)]
+        m = len(board)
+        n = len(board[0])
 
-        def dfs(row, col):
-            print(f"dfs: {row}, {col}")
-            visited[row][col] = 1
+        visited = [[0 for _ in range(n)] for _ in range(m)]
+
+        for i in range(m):
+            for j in range(n):
+                if (i == 0 or i == m - 1 or j == 0 or j == n - 1) and  board[i][j] == "O" and visited[i][j] != 1:
+                    self.dfs(i, j, board, visited, m, n)
+
+
+        for i in range(m):
+            for j in range(n):
+                if board[i][j] == "O" and visited[i][j] == 0:
+                    board[i][j] = "X"
+        
+
+    def dfs(self, row, col, board, visited, m, n):
+        stack = []
+
+        visited[row][col] = 1
+        stack.append((row, col))
+
+        directions = [(0, 1), (1, 0), (-1, 0), (0, -1)]
+
+        while stack:
+            r, c = stack.pop()
 
             for dr, dc in directions:
-                nr = row + dr
-                nc = col + dc
-                if 0 <= nr < row_len and 0 <= nc < col_len and board[nr][nc] == 'O' and visited[nr][nc] != 1:
-                    dfs(nr, nc)
-
-        for i in range(row_len):
-            for j in range(col_len):
-                if (i == 0 or i == row_len - 1 or j == 0 or j == col_len - 1) and board[i][j] == 'O' and visited[i][j] != 1:
-                    dfs(i, j)
-
-        print("Vis: ", visited)
-
-        for i in range(row_len):
-            for j in range(col_len):
-                if board[i][j] == 'O' and visited[i][j] != 1:
-                    board[i][j] = 'X'
-                
-
-
+                nr = r + dr
+                nc = c + dc
+                if 0 <= nr < m and 0 <= nc < n and board[nr][nc] == "O" and visited[nr][nc] != 1:
+                    stack.append((nr, nc))
+                    visited[nr][nc] = 1
         
