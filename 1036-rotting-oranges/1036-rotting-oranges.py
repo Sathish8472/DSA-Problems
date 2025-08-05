054c94d84e46dbe7e0b48db6b2b1d7b3e1b6798f
Sathish <sathish8472@gmail.com>
2025-08-05 21:54:30 +0100

    Time: 0 ms (100%), Space: 17.8 MB (71.69%) - LeetHub

--- 1036-rotting-oranges/1036-rotting-oranges.py
+++ 1036-rotting-oranges/1036-rotting-oranges.py
@@ -1,42 +1,39 @@
 class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
-        row_len = len(grid)
-        col_len = len(grid[0])
-        
-        rotten_oranges = deque()
+        m = len(grid)
+        n = len(grid[0])
+        queue = deque()
         fresh_oranges = 0
 
-        for r in range(row_len):
-            for c in range(col_len):
-                if grid[r][c] == 2:
-                    rotten_oranges.append((r, c))
-                elif grid[r][c] == 1:
+        for i in range(m):
+            for j in range(n):
+                if grid[i][j] == 1:
                     fresh_oranges += 1
-
-        print("fresh_oranges: ", fresh_oranges)
+                elif grid[i][j] == 2:
+                    queue.append((i, j, 0))
 
         if fresh_oranges == 0:
             return 0
+
+        time = 0
+        directions = [(0, 1), (1, 0), (0, -1), (-1, 0)]
+
+        while queue:
+            row, col, t = queue.popleft()
+            time = max(time, t)
+
+            for dr, dc in directions:
+                nr = row + dr
+                nc = col + dc
+
+                if 0 <= nr < m and 0 <= nc < n and grid[nr][nc] == 1:
+                    grid[nr][nc] = 2
+                    fresh_oranges -= 1
+                    queue.append((nr, nc, t + 1))
+
+        if fresh_oranges != 0:
+            return -1
+
+        return time
+
         
-        minutes = 0
-        directions = [(0, 1), (0, -1), (1, 0), (-1, 0)]
-
-        while rotten_oranges:
-            minutes += 1
-
-            for _ in range(len(rotten_oranges)):
-                r, c = rotten_oranges.popleft()
-
-                for dr, dc in directions:
-                    nr = r + dr
-                    nc = c + dc
-
-                    if 0 <= nr < row_len and 0 <= nc < col_len and grid[nr][nc] == 1:
-                        grid[nr][nc] = 2
-                        fresh_oranges -= 1
-                        rotten_oranges.append((nr, nc))
-            
-            if fresh_oranges == 0:
-                return minutes
-
-        return -1
