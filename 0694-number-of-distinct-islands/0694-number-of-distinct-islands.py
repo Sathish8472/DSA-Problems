5a248657cf3e4a11e89a50b0bf1cdf0e8078b41c
Sathish <sathish8472@gmail.com>
2025-08-09 23:55:24 +0100

    Time: 25 ms (96.16%), Space: 18.2 MB (92.26%) - LeetHub

--- 0694-number-of-distinct-islands/0694-number-of-distinct-islands.py
+++ 0694-number-of-distinct-islands/0694-number-of-distinct-islands.py
@@ -4,26 +4,43 @@ class Solution:
         col_len = len(grid[0])
         
         visited = [[0] * col_len for _ in range(row_len)]
-        directions = [(0, 1), (1, 0), (0, -1), (-1, 0)]
+        self.directions = [(0, 1), (1, 0), (0, -1), (-1, 0)]
+
         distinct_islands = set()
-
-        def dfs(row, col, base_row, base_col, path):
-            path.append((row - base_row, col - base_col))
-            visited[row][col] = 1
-
-            for dr, dc in directions:
-                nr = row + dr
-                nc = col + dc
-
-                if 0 <= nr < row_len and 0 <= nc < col_len and grid[nr][nc] == 1 and visited[nr][nc] != 1:
-                    dfs(nr, nc, base_row, base_col, path)
-
         for r in range(row_len):
             for c in range(col_len):
                 if grid[r][c] == 1 and visited[r][c] != 1:
                     path = []
-                    visited[r][c] = 1
-                    dfs(r, c, r, c, path)
+                    # self.dfs(r, c, r, c, grid, visited, path, row_len, col_len)
+                    self.bfs(r, c, r, c, grid, visited, path, row_len, col_len)
                     distinct_islands.add(tuple(path))
 
         return len(distinct_islands)
+
+    def dfs(self, row, col, base_row, base_col, grid, visited, path, row_len, col_len):
+            path.append((row - base_row, col - base_col))
+            visited[row][col] = 1
+
+            for dx, dy in self.directions:
+                nr = row + dx
+                nc = col + dy
+
+                if 0 <= nr < row_len and 0 <= nc < col_len and grid[nr][nc] == 1 and visited[nr][nc] != 1:
+                    self.dfs(nr, nc, base_row, base_col, grid, visited, path, row_len, col_len)
+    
+    def bfs(self, row, col, base_row, base_col, grid, visited, path, row_len, col_len):
+        queue = deque()
+        queue.append((row, col))
+        visited[row][col] = 1
+
+        while queue:
+            r, c = queue.popleft()
+            path.append((r - base_row, c - base_col))
+
+            for dx, dy in self.directions:
+                nr = r + dx
+                nc = c + dy
+                if 0 <= nr < row_len and 0 <= nc < col_len and grid[nr][nc] == 1 and visited[nr][nc] != 1:
+                    visited[nr][nc] = 1
+                    queue.append((nr, nc))
+
