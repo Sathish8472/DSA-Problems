7d196fb4299f9ee4d8fade9bd6d67cfa9976a3d7
Sathish <sathish8472@gmail.com>
2025-08-09 23:36:11 +0100

    Time: 19 ms (70.35%), Space: 18 MB (92.88%) - LeetHub

--- 0695-max-area-of-island/0695-max-area-of-island.py
+++ 0695-max-area-of-island/0695-max-area-of-island.py
@@ -7,25 +7,50 @@ class Solution:
 
         island_max_area = 0
 
-        directions = [(0, 1), (1, 0), (0, -1), (-1, 0)]
-
-        def dfs(row, col):
-            visited[row][col] = 1
-            area = 1
-
-            for dr, dc in directions:
-                nr = row + dr
-                nc = col + dc
-
-                if 0 <= nr < row_len and 0 <= nc < col_len and grid[nr][nc] == 1 and visited[nr][nc] != 1:
-                    area += dfs(nr, nc)
-
-            return area
+        self.directions = [(0, 1), (1, 0), (0, -1), (-1, 0)]
 
         for r in range(row_len):
             for c in range(col_len):
                 if grid[r][c] == 1 and visited[r][c] != 1:
-                    area = dfs(r, c)
+                    # area = self.dfs(r, c, visited, grid, row_len, col_len)
+                    area = self.bfs(r, c, visited, grid, row_len, col_len)
                     island_max_area = max(island_max_area, area)
 
         return island_max_area
+
+    def dfs(self, row, col, visited, grid, row_len, col_len):
+        visited[row][col] = 1
+        area = 1
+
+        for dr, dc in self.directions:
+            nr = row + dr
+            nc = col + dc
+
+            if (
+                0 <= nr < row_len
+                and 0 <= nc < col_len
+                and grid[nr][nc] == 1
+                and visited[nr][nc] != 1
+            ):
+                area += self.dfs(nr, nc, visited, grid, row_len, col_len)
+
+        return area
+
+    def bfs(self, row, col, visited, grid, row_len, col_len):
+        queue = deque()
+        queue.append((row, col))
+        visited[row][col] = 1
+        area = 1
+
+        while queue:
+            r, c = queue.popleft()
+
+            for dx, dy in self.directions:
+                nr = r + dx
+                nc = c + dy
+                if 0 <= nr < row_len and 0 <= nc < col_len and grid[nr][nc] == 1 and visited[nr][nc] != 1:
+                    visited[nr][nc] = 1
+                    queue.append((nr, nc))
+                    area += 1
+
+        return area
