1061acd07e2c7d85f36aeefecf4ecbf3f0ba9961
Sathish <sathish8472@gmail.com>
2025-08-22 14:42:24 +0100

    Time: 0 ms (100%), Space: 17.8 MB (74.44%) - LeetHub

--- 0050-powx-n/0050-powx-n.py
+++ 0050-powx-n/0050-powx-n.py
@@ -1,17 +1,32 @@
 class Solution:
     def myPow(self, x: float, n: int) -> float:
+
+        if n == 0:
+            return 1.0
+        
+        if n == 1:
+            return x
+        
+        if n < 0:
+            return 1.0 / self.myPow(x, -n)
+
+        if n % 2 == 0:
+            half_pow = self.myPow(x, n // 2)
+            return half_pow * half_pow
+        else:
+            return x * self.myPow(x, n - 1)
+    
+    def myPow1(self, x: float, n: int) -> float:
+        if x == 1.0 or n == 0:
+            return 1
+            
+        temp = n
         if n < 0:
             x = 1 / x
-            n = -n
+            temp = -1 * n
+
+        ans = 1
+        for i in range(temp):
+            ans *= x
         
-        def fastPow(x, n):
-            if n == 0:
-                return 1
-            
-            half = fastPow(x, n // 2)
-            if n % 2 == 0:
-                return half * half
-            else:
-                return half * half * x
-    
-        return fastPow(x, n)
+        return ans
