d4cba37941193529604e4ad26a11c443cad74b06
Sathish <sathish8472@gmail.com>
2025-08-26 11:03:16 +0100

    Time: 34 ms (12.38%), Space: 17.7 MB (83.86%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.py
+++ 0040-combination-sum-ii/0040-combination-sum-ii.py
@@ -3,37 +3,10 @@ class Solution:
         ans = []
         candidates.sort()
 
-        self.func(0, [], target, candidates, ans)
-
+        self.func(0, target, [], candidates, ans)
         return ans
 
-    def func(self, ind, current_sub, current_sum, candidates, result):
-        if current_sum == 0:
-            result.append(current_sub[:])
-            return
-        
-        if ind == len(candidates) or current_sum < 0:
-            return
-
-        current_sub.append(candidates[ind])
-        take = self.func(ind + 1, current_sub, current_sum - candidates[ind], candidates, result)
-        current_sub.pop()
-
-        for i in range(ind + 1, len(candidates)):
-            if candidates[i] != candidates[ind]:
-                self.func(i, current_sub, current_sum, candidates, result)
-                break
-        return
-
-
-    def combinationSum211(self, candidates: List[int], target: int) -> List[List[int]]:
-        ans = []
-        candidates.sort()
-
-        self.func1(0, target, [], candidates, ans)
-        return ans
-
-    def func1(self, ind, current_sum, current_sub, nums, result):
+    def func(self, ind, current_sum, current_sub, nums, result):
         if current_sum < 0:
             return
 
