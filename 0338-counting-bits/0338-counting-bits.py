f420d205e26f749b03afddafe0f9f3ee0ee60932
Sathish <sathish8472@gmail.com>
2025-09-03 18:17:38 +0100

    Time: 11 ms (39.71%), Space: 18.6 MB (58.76%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.py
+++ 0338-counting-bits/0338-counting-bits.py
@@ -1,29 +1,13 @@
 class Solution:
+    def countBits(self, n: int) -> List[int]:
 
-    def countBits(self, n : int) -> List[int]:
-        ans = [0] * (n + 1)
+        result = []
+
+        result.append(0)
 
         for i in range(1, n + 1):
-            ans[i] = ans[i >> 1] + (i & 1)  # Right shift + last bit check
+            count = bin(i).count('1')
+            result.append(count)
         
-        return ans
-
+        return result
         
-
-    # Brute
-    # time: O(n log n), Space: O(N)
-    def countBits_1(self, n: int) -> List[int]:
-        ans = [0] * (n + 1)
-
-        def getCount(num):
-            count = 0
-            while num > 0:
-                count += 1
-                num = num & (num - 1)
-            return count
-
-        for i in range(len(ans)):
-            count = getCount(i)
-            ans[i] = count
-
-        return ans
