85c7830e96fca0b97fc97b6a018d8e5c24f8d59c
Sathish <sathish8472@gmail.com>
2025-09-03 18:29:56 +0100

    Time: 3 ms (94.13%), Space: 18.6 MB (58.76%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.py
+++ 0338-counting-bits/0338-counting-bits.py
@@ -1,8 +1,41 @@
 class Solution:
+    # Optimized DP with LSB Formula
     def countBits(self, n: int) -> List[int]:
+        result = [0] * (n + 1)
 
+        for i in range(1, n + 1):
+            result[i] = result[i >> 1] + (i & 1)
+        
+        return result
+
+
+    # Bitwise Right Shift
+    def countBits3(self, n: int) -> List[int]:
+        result = [0] * (n + 1)
+
+        for i in range(1, n + 1):
+            if i & 1 == 0:
+                result[i] = result[i >> 1]
+            else:
+                result[i] = result[i >> 1] + 1
+            
+        return result
+    
+    # Division 
+    def countBits2(self, n: int) -> List[int]:
+        result = [0] * (n + 1)
+
+        for i in range(1, n + 1):
+            if i & 1 == 0:
+                result[i] = result[i // 2]
+            else:
+                result[i] = result[i // 2] + 1
+            
+        return result
+
+    # Brute Force with Built-in Function - n Log n
+    def countBits1(self, n: int) -> List[int]:
         result = []
-
         result.append(0)
 
         for i in range(1, n + 1):
