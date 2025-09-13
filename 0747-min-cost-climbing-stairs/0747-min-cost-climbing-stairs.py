53cc1a2a4b131f1751b1dd754f5cbbf789241a07
Sathish <sathish8472@gmail.com>
2025-09-13 10:38:10 +0100

    Time: 9 ms (7.38%), Space: 19.2 MB (15.66%) - LeetHub

--- 0747-min-cost-climbing-stairs/0747-min-cost-climbing-stairs.py
+++ 0747-min-cost-climbing-stairs/0747-min-cost-climbing-stairs.py
@@ -1,6 +1,27 @@
 class Solution:
     def minCostClimbingStairs(self, cost: List[int]) -> int:
         n = len(cost)
+
+        dp = [-1] * (n + 1)
+
+        def _helper(ind):
+            if ind >= len(cost):
+                return 0
+            
+            if dp[ind] != -1:
+                return dp[ind]
+
+            jumpOne = cost[ind] + _helper(ind + 1)
+            jumpTwo = cost[ind] + _helper(ind + 2)
+
+            dp[ind] = min(jumpOne, jumpTwo)
+            return dp[ind]
+
+        return min(_helper(0), _helper(1))
+
+
+    def minCostClimbingStairs1(self, cost: List[int]) -> int:
+        n = len(cost)
     
         def _helper(ind):
             if ind >= len(cost):
