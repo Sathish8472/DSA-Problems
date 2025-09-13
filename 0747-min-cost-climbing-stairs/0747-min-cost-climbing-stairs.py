fce3d3d5c5e6d9d56a68b52185f23114e3894106
Sathish <sathish8472@gmail.com>
2025-09-13 11:01:26 +0100

    Time: 2 ms (78.86%), Space: 18.1 MB (20.03%) - LeetHub

--- 0747-min-cost-climbing-stairs/0747-min-cost-climbing-stairs.py
+++ 0747-min-cost-climbing-stairs/0747-min-cost-climbing-stairs.py
@@ -1,7 +1,40 @@
 class Solution:
+
+    # Space Optimization
+    # Time: O(N), Space: O(1)
     def minCostClimbingStairs(self, cost: List[int]) -> int:
         n = len(cost)
+        
+        prev2 = cost[0]
+        prev1 = cost[1]
 
+        for i in range(2, n):
+            curi = cost[i] + min(prev1, prev2)
+            prev2, prev1 = prev1, curi
+        
+        return min(prev1, prev2)
+
+
+
+    # Tabulation (Bottom-Up DP)
+    # Time: O(N), Space: O(N)
+    def minCostClimbingStairs3(self, cost: List[int]) -> int:
+        n = len(cost)
+        dp = [-1] * (n + 1)
+
+        dp[0] = cost[0]
+        dp[1] = cost[1]
+
+        for i in range(2, n):
+            dp[i] = cost[i] + min(dp[i - 1], dp[i - 2])
+        
+        return min(dp[n - 1], dp[n - 2])
+
+
+    # Recursion + Memoization (Top-Down DP)
+    # Time: O(N), Space: O(N) + O(N) (Stack + dp array)
+    def minCostClimbingStairs2(self, cost: List[int]) -> int:
+        n = len(cost)
         dp = [-1] * (n + 1)
 
         def _helper(ind):
@@ -20,6 +53,7 @@ class Solution:
         return min(_helper(0), _helper(1))
 
 
+    # Recursion => Time: O(2 ^ n), Space: O(N)
     def minCostClimbingStairs1(self, cost: List[int]) -> int:
         n = len(cost)
     
@@ -33,4 +67,3 @@ class Solution:
             return min(jumpOne, jumpTwo)
 
         return min(_helper(0), _helper(1))
-        
