df41697b07b588d663dab47276179ef21bda5be5
Sathish <sathish8472@gmail.com>
2025-09-13 12:26:24 +0100

    Time: 70 ms (77.42%), Space: 60.7 MB (82.08%) - LeetHub

--- 2262-solving-questions-with-brainpower/2262-solving-questions-with-brainpower.py
+++ 2262-solving-questions-with-brainpower/2262-solving-questions-with-brainpower.py
@@ -1,44 +1,58 @@
 class Solution:
-
-
-    # DP - Memoization 
-    # Time: O(N)
-    # Space: O(N)
+    
+    # Tabulation, Time: O(N), Space: O(N)
     def mostPoints(self, questions: List[List[int]]) -> int:
-
-        dp = [-1] * (len(questions) + 1)
-        return self._solve(questions, 0, dp)
+        n = len(questions)
+        dp = [0] * (n + 1)
         
-    def _solve(self, questions, ind, dp) -> int:
-        if ind >= len(questions):
-            return 0
+        for i in range(n - 1, -1, -1):
+            point, bp = questions[i]
+            solve = point + (dp[i + bp + 1] if i + bp + 1 < n else 0)
+            skip = dp[i + 1]
 
-        if dp[ind] != -1:
+            dp[i] = max(solve, skip)
+
+        return dp[0]
+
+    # Memoization, Time: O(N), Space: O(N) + O(N) stack and dp array
+    def mostPoints2(self, questions: List[List[int]]) -> int:
+        n = len(questions)
+        dp = [-1] * (n + 1)
+
+        def _helper(ind):
+            if ind >= n:
+                return 0
+            
+            if dp[ind] != -1:
+                return dp[ind]
+
+            point, brainPower = questions[ind] 
+
+            solve = point + _helper(ind + brainPower + 1)
+            skip = _helper(ind + 1)
+
+            dp[ind] = max(solve, skip)
             return dp[ind]
 
-        quest = questions[ind]
-        solve = quest[0] + self._solve(questions, ind + quest[1] + 1, dp)
-        skip = 0 + self._solve(questions, ind + 1, dp)
-
-        dp[ind] = max(solve, skip)
-        
-        return dp[ind]
+        return _helper(0)
 
 
-    
-    # Recursion
-    # Time: 2 ^ n, exponential time
-    # Space: O(N) recursion stack space
-    def mostPoints_1(self, questions: List[List[int]]) -> int:
-        return self._solve(questions, 0)
-        
-    def _solve2(self, questions, ind) -> int:
-        if ind >= len(questions):
-            return 0
+    # Recursion, Time: 2 ^ n, Space: O(N) stack
+    def mostPoints1(self, questions: List[List[int]]) -> int:
+        n = len(questions)
 
-        quest = questions[ind]
-        solve = quest[0] + self._solve(questions, ind + quest[1] + 1)
-        skip = 0 + self._solve(questions, ind + 1)
+        def _helper(ind):
+            if ind >= n:
+                return 0
 
-        return max(solve, skip)
+            point, brainPower = questions[ind] 
+
+            solve = point + _helper(ind + brainPower + 1)
+            skip = _helper(ind + 1)
+
+            return max(solve, skip)
+
+        return _helper(0)
+
+
         
