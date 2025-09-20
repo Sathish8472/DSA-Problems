6726ec85a07743511485038f01056a79f83bb699
Sathish <sathish8472@gmail.com>
2025-09-20 21:56:48 +0100

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0120-triangle/0120-triangle.py
@@ -0,0 +1,21 @@
+class Solution:
+    def minimumTotal(self, triangle: List[List[int]]) -> int:
+        return self.solve(0, 0, triangle)
+    
+    def solve(self, i, j, triangle):
+        n = len(triangle)
+        
+        if i == n - 1:
+            return triangle[i][j]
+
+        if i >= len(triangle):
+            return float('inf')
+        
+        if j < 0 or j >= len(triangle[i]):
+            return float('inf')
+        
+        left_dg = self.solve(i + 1, j - 1, triangle)
+        bottom = self.solve(i + 1, j, triangle)
+        right_dg = self.solve(i + 1, j + 1, triangle)
+
+        return triangle[i][j] + min(left_dg, bottom, right_dg)
