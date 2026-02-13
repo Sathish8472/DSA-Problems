f13738d26dc06341f5c644ab4783b141d30a6575
Sathish <sathish8472@gmail.com>
2026-02-13 17:20:26 +0000

    Time: 2082 ms (40.15%), Space: 133.9 MB (12.41%) - LeetHub

--- 3714-longest-balanced-substring-ii/3714-longest-balanced-substring-ii.py
+++ 3714-longest-balanced-substring-ii/3714-longest-balanced-substring-ii.py
@@ -10,13 +10,13 @@ class Solution:
         m = {}
         for i, (a, b, c) in enumerate(p):
             for k in [
-                (-1, a - b, a - c), # a,b,c
-                (-2, a - b, c),     # a,b
-                (-3, b - c, a),     # b,c
-                (-4, c - a, b),     # a,c
-                (-5, b, c),         # a
-                (-6, c, a),         # b
-                (-7, a, b),         # c
+                (-1, a - b, a - c), 
+                (-2, a - b, c),    
+                (-3, b - c, a),  
+                (-4, c - a, b),    
+                (-5, b, c),       
+                (-6, c, a),       
+                (-7, a, b),       
             ]:
                 if not k in m: m[k] = i
                 else: ans = max(ans, i - m[k])
